0f16f04c42970638c491c61caaaac9beb2ae0bfd
allenmoatallen <allenmoatallen@64e5170e-faa2-406b-82c6-f559128ac820>
2012-07-12 06:07:05 +0000

    Committing skeletal ShaderManager class.

---
+++ core/include/NAS2D/Renderer/ShaderManager.h
@@ -0,0 +1,14 @@
+//
+//  ShaderManager.h
+//  NAS2D
+//
+//  Created by Trevor Allen on 7/11/12.
+//
+//
+
+#ifndef __NAS2D__ShaderManager__
+#define __NAS2D__ShaderManager__
+
+#include <iostream>
+
+#endif /* defined(__NAS2D__ShaderManager__) */
---
+++ core/src/Renderer/ShaderManager.cpp
@@ -0,0 +1,9 @@
+//
+//  ShaderManager.cpp
+//  NAS2D
+//
+//  Created by Trevor Allen on 7/11/12.
+//
+//
+
+#include "NAS2D/Renderer/ShaderManager.h"

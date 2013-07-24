0af4024670a89e93ba1e30234341296b7cf9362a
leeornet <leeornet@64e5170e-faa2-406b-82c6-f559128ac820>
2013-07-24 14:29:31 +0000

    - Updated copyright notice in all NAS2D core files.

--- core/trunk/include/NAS2D/KeyTranslator.h
+++ core/trunk/include/NAS2D/KeyTranslator.h
@@ -1,6 +1,6 @@
 // ==================================================================================
 // = NAS2D
-// = Copyright © 2008 - 2012 New Age Software
+// = Copyright © 2008 - 2013 New Age Software
 // ==================================================================================
 // = NAS2D is distributed under the terms of the zlib license. You are free to copy,
 // = modify and distribute the software under the terms of the zlib license.

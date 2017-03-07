f12108d8e7f498e3ff9b61b3f6bd0c3f78b04a54
Leeor Dicker <ldicker83@users.noreply.github.com>
2017-03-07 15:57:37 -0500

    Updated KeyTranslator documentation.

--- include/NAS2D/KeyTranslator.h
+++ include/NAS2D/KeyTranslator.h
@@ -1,6 +1,6 @@
 // ==================================================================================
 // = NAS2D
-// = Copyright © 2008 - 2017 New Age Software
+// = Copyright Â© 2008 - 2017 New Age Software
 // ==================================================================================
 // = NAS2D is distributed under the terms of the zlib license. You are free to copy,
 // = modify and distribute the software under the terms of the zlib license.
@@ -17,6 +17,25 @@ namespace NAS2D {
 /**
  * \class KeyTranslator
  * \brief Translates key events into a single alpha-numeric character.
+ * 
+ * The KeyTranslator class is a quick and easy way to translate keyboard
+ * events into alphanumeric characters. This is usefull in cases where you
+ * need text input without needing large if/then chains or a large switch
+ * statement.
+ * 
+ * \note	KeyTranslator is a static class and therefore should never
+ * 		be instantiated by itself.
+ * 
+ * Using the KeyTranslator is simple. Simply call its static methods with
+ * the parameters from your KeyDown event handler:
+ * 
+ * \code{.cpp}
+ * void onKeyDown(KeyCode key, KeyModifer mod, bool repeat)
+ * {
+ * 	std::string character = KeyTranslator::translate(key, mod);
+ 	std::cout << character << std::endl;
+ * }
+ *  \endcode
  */
 class KeyTranslator
 {

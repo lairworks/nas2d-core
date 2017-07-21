c335716c8b7658498c26f75fc85da7e43905e127
ldicker83 <leeor_net@yahoo.com>
2017-07-21 06:28:39 -0400

    Commented out Shader.cpp as it hasn't been updated in 5 years and doesn't conform to the quality of the rest of the code.

--- include/NAS2D/Resources/Shader.h
+++ include/NAS2D/Resources/Shader.h
@@ -1,10 +1,17 @@
-//
+// ==================================================================================
+// = NAS2D
+// = Copyright © 2008 - 2017 New Age Software
+// ==================================================================================
+// = NAS2D is distributed under the terms of the zlib license. You are free to copy,
+// = modify and distribute the software under the terms of the zlib license.
+// =
+// = Acknowledgement of your use of NAS2D is appriciated but is not required.
+// ==================================================================================
+
 //  Shader.h
 //  NAS2D
 //
 //  Created by Trevor Allen on 7/30/12.
-//
-//
 
 #pragma once
 
--- src/Resources/Shader.cpp
+++ src/Resources/Shader.cpp
@@ -1,12 +1,21 @@
-//
-//  Shader.cpp
+// ==================================================================================
+// = NAS2D
+// = Copyright © 2008 - 2017 New Age Software
+// ==================================================================================
+// = NAS2D is distributed under the terms of the zlib license. You are free to copy,
+// = modify and distribute the software under the terms of the zlib license.
+// =
+// = Acknowledgement of your use of NAS2D is appriciated but is not required.
+// ==================================================================================
+
+//  Shader.h
 //  NAS2D
 //
 //  Created by Trevor Allen on 7/30/12.
-//
-//
 
+/*
 #include "NAS2D/Resources/Shader.h"
+
 
 #ifdef _WIN32
 #include <windows.h>
@@ -223,3 +232,5 @@ private:
 	map<string, GLuint> m_uniformMap;
 	map<string, GLuint> m_attribMap;
 };
+
+*/

bbd061cd00ab51e1f520c59754e46d4e5b6c46cd
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-03-26 20:43:11 +0700

    Add Utility::init method
    New method is able to construct objects of either base or derived type
    in place, using forwaded constructor arguments.
    
    This eliminates concerns about double assignment, as the object is
    constructed internally, so it's guaranteed not to reference an existing
    object.

--- include/NAS2D/Utility.h
+++ include/NAS2D/Utility.h
@@ -65,6 +65,49 @@ public:
 
 
 	/**
+	 * Creates a \c Utility<T> interface for a newly created object of type
+	 * \c Type. \c Type must be derived from type \c T specified in the
+	 * template class argument list, and defaults to \c T if not explicitly
+	 * specified.
+	 *
+	 * \c Type should be explicitly specified when instantiating a derived type
+	 * and polymorphic behavior is desired.
+	 *
+	 * \code{.cpp}
+	 * // Instantiate Mixer (default Type is Mixer)
+	 * Utility<Mixer>::init();
+	 * \endcode
+	 *
+	 * \code{.cpp}
+	 * // Instantiate derived type Mixer_SDL, of base type Mixer
+	 * Utility<Mixer>::init<Mixer_SDL>();
+	 * \endcode
+	 *
+	 * \code{.cpp}
+	 * // Instantiate derived type with constructor arguments, as base type reference
+	 * Utility<Base>::init<Derived>(arg1, arg2, arg3);
+	 * \endcode
+	 *
+	 * \param	args	A list of arguments to be forwarded to the \c Type objects's constructor
+	 *
+	 * \note	This method should be called before <tt>Utility::get()</tt>.
+	 */
+	template<typename Type = T, typename... Args>
+	static void init(Args&&... args)
+	{
+		// Instantiate a new object with forwarded constructor arguments
+		auto newInstance = new Type(std::forward<Args>(args)...);
+
+		if (mInstance)
+		{
+			delete mInstance;
+		}
+
+		mInstance = newInstance;
+	}
+
+
+	/**
 	 * Creates a \c Utility<T> interface using a derived type \c T* for
 	 * derived implementation. This is useful when you need polymorphic
 	 * behavior in an object that you need global access to.

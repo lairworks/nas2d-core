e6c7998d8ac2228f91e13a7ed86d131ac7635cbc
cugone <cugone@gmail.com>
2019-04-06 22:49:11 -0500

    Added PLATFORM_LINUX preproc define;

--- include/NAS2D/BuildConfig.h
+++ include/NAS2D/BuildConfig.h
@@ -2,11 +2,16 @@
 
 #if defined(__clang__)
 #define PLATFORM_CLANG
+#define PLATFORM_LINUX
 #elif defined(__GNUC__) || defined(__GNUG__)
 #define PLATFORM_GNUC
+#define PLATFORM_LINUX
 #endif
 
 #if defined(__APPLE__)
+#ifdef PLATFORM_LINUX
+#undef PLATFORM_LINUX
+#endif
 #define PLATFORM_APPLE
 #elif defined(_MSC_VER) || defined(_WIN32) || defined(_WIN64)
 #define PLATFORM_WINDOWS

c24ad3d5de96d75db85da9ece2a26fb5e9409e66
cugone <cugone@gmail.com>
2019-04-07 02:11:40 -0500

    Unconditional #undef PLATFORM_LINUX when Apple compiler is detected.

--- include/NAS2D/BuildConfig.h
+++ include/NAS2D/BuildConfig.h
@@ -9,9 +9,7 @@
 #endif
 
 #if defined(__APPLE__)
-#ifdef PLATFORM_LINUX
 #undef PLATFORM_LINUX
-#endif
 #define PLATFORM_APPLE
 #elif defined(_MSC_VER) || defined(_WIN32) || defined(_WIN64)
 #define PLATFORM_WINDOWS

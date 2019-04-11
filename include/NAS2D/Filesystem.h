9a49cc72f4c8c195d25c46c614fdf7cf579bea00
cugone <cugone@gmail.com>
2019-04-11 14:39:32 -0500

    Apple compile hacks

--- include/NAS2D/Filesystem.h
+++ include/NAS2D/Filesystem.h
@@ -14,17 +14,6 @@
 
 //Hacky fix until Apple gets off its butt.
 #if defined(PLATFORM_APPLE)
-// ==================================================================================
-// = NAS2D
-// = Copyright © 2008 - 2018 New Age Software
-// ==================================================================================
-// = NAS2D is distributed under the terms of the zlib license. You are free to copy,
-// = modify and distribute the software under the terms of the zlib license.
-// = 
-// = Acknowledgement of your use of NAS2D is appriciated but is not required.
-// ==================================================================================
-
-#pragma once
 
 #include "Common.h"
 #include "File.h"

17c6cb520006ce752e9fdfd687c3cec1066e494d
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-05-05 23:58:24 +0700

    Add `MixerNull` class

---
+++ include/NAS2D/Mixer/MixerNull.h
@@ -0,0 +1,47 @@
+// ==================================================================================
+// = NAS2D
+// = Copyright © 2008 - 2019 New Age Software
+// ==================================================================================
+// = NAS2D is distributed under the terms of the zlib license. You are free to copy,
+// = modify and distribute the software under the terms of the zlib license.
+// =
+// = Acknowledgement of your use of NAS2D is appriciated but is not required.
+// ==================================================================================
+
+#pragma once
+
+#include "Mixer.h"
+
+namespace NAS2D {
+
+class MixerNull : public Mixer
+{
+public:
+	MixerNull() = default;
+	~MixerNull() = default;
+
+	// Sound Functions
+	void playSound(Sound& sound) override {}
+	void stopSound() override {}
+	void pauseSound() override {}
+	void resumeSound() override {}
+
+	// Music Functions
+	void stopMusic() override {}
+	void pauseMusic() override {}
+	void resumeMusic() override {}
+
+	void fadeInMusic(Music& music, int loops, int time) override {}
+	void fadeOutMusic(int time) override {}
+
+	bool musicPlaying() const override { return false; }
+
+	// Global Functions
+	void soundVolume(int level) override {}
+	void musicVolume(int level) override {}
+
+	void mute() override {}
+	void unmute() override {}
+};
+
+}

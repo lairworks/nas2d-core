1dfced55a0d340b40b936ac3c545586040c54e92
cugone <cugone@gmail.com>
2019-05-05 12:02:35 -0500

    Convert Mixer to Abstract Data Type
    Converted Mixer to an Abstract Data Type.
    It contains data and non-virtual member functions
    so calling it an "interface" is a misnomer.
    
    Derived NullMixer from Mixer ADT
    
    Updated Non-compiling usage of plain Mixer to
    NullMixer when audio driver fails to load.
    
    Added NullMixer header and implementation to project files.

---
+++ include/NAS2D/Mixer/NullMixer.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "NAS2D/Mixer/Mixer.h"
+
+namespace NAS2D
+{
+
+	class NullMixer : public Mixer
+	{
+	  public:
+		NullMixer() = default;
+		NullMixer(const NullMixer& other) = default;
+		NullMixer(NullMixer&& other) = default;
+		NullMixer& operator=(const NullMixer& other) = default;
+		NullMixer& operator=(NullMixer&& other) = default;
+		virtual ~NullMixer() = default;
+
+
+virtual void playSound(Sound& sound) override;
+
+
+virtual void stopSound() override;
+
+
+virtual void pauseSound() override;
+
+
+virtual void resumeSound() override;
+
+
+virtual void stopMusic() override;
+
+
+virtual void pauseMusic() override;
+
+
+virtual void resumeMusic() override;
+
+
+virtual void fadeInMusic(Music& music, int loops = Mixer::CONTINUOUS, int time = Mixer::DEFAULT_FADE_TIME) override;
+
+
+virtual void fadeOutMusic(int time = Mixer::DEFAULT_FADE_TIME) override;
+
+
+virtual bool musicPlaying() const override;
+
+
+virtual void mute() override;
+
+
+virtual void unmute() override;
+
+
+virtual void soundVolume(int level) override;
+
+
+virtual void musicVolume(int level) override;
+
+protected:
+private:
+	};
+
+} // namespace NAS2D

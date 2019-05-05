0b4eb87e1e8961b7d1f4eecff50fab3785163e03
cugone <cugone@gmail.com>
2019-05-05 12:20:35 -0500

    Format Document for consistency

--- include/NAS2D/Mixer/NullMixer.h
+++ include/NAS2D/Mixer/NullMixer.h
@@ -15,50 +15,36 @@ namespace NAS2D
 		NullMixer& operator=(NullMixer&& other) = default;
 		virtual ~NullMixer() = default;
 
+		virtual void playSound(Sound& sound) override;
 
-virtual void playSound(Sound& sound) override;
+		virtual void stopSound() override;
 
+		virtual void pauseSound() override;
 
-virtual void stopSound() override;
+		virtual void resumeSound() override;
 
+		virtual void stopMusic() override;
 
-virtual void pauseSound() override;
+		virtual void pauseMusic() override;
 
+		virtual void resumeMusic() override;
 
-virtual void resumeSound() override;
+		virtual void fadeInMusic(Music& music, int loops = Mixer::CONTINUOUS, int time = Mixer::DEFAULT_FADE_TIME) override;
 
+		virtual void fadeOutMusic(int time = Mixer::DEFAULT_FADE_TIME) override;
 
-virtual void stopMusic() override;
+		virtual bool musicPlaying() const override;
 
+		virtual void mute() override;
 
-virtual void pauseMusic() override;
+		virtual void unmute() override;
 
+		virtual void soundVolume(int level) override;
 
-virtual void resumeMusic() override;
+		virtual void musicVolume(int level) override;
 
-
-virtual void fadeInMusic(Music& music, int loops = Mixer::CONTINUOUS, int time = Mixer::DEFAULT_FADE_TIME) override;
-
-
-virtual void fadeOutMusic(int time = Mixer::DEFAULT_FADE_TIME) override;
-
-
-virtual bool musicPlaying() const override;
-
-
-virtual void mute() override;
-
-
-virtual void unmute() override;
-
-
-virtual void soundVolume(int level) override;
-
-
-virtual void musicVolume(int level) override;
-
-protected:
-private:
+	  protected:
+	  private:
 	};
 
 } // namespace NAS2D

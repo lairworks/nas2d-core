d001ae4cf11b100d21001b23d45b650c1b858bbc
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-05-06 23:13:33 +0700

    Add FilesystemTest fixture and tests
    This demonstrates how to write tests using a common test fixture.

--- test/Filesystem.test.cpp
+++ test/Filesystem.test.cpp
@@ -11,3 +11,25 @@ TEST(Filesystem, Init) {
 	NAS2D::Filesystem fs;
 	EXPECT_NO_THROW(fs.init("", "NAS2DUnitTests", "LairWorks", "./"));
 }
+
+
+class FilesystemTest : public ::testing::Test {
+protected:
+	void SetUp() override {
+		fs.init("", "NAS2DUnitTests", "LairWorks", "data/");
+	}
+
+	NAS2D::Filesystem fs;
+};
+
+
+TEST_F(FilesystemTest, dataPath) {
+	EXPECT_EQ("data/", fs.dataPath());
+}
+
+TEST_F(FilesystemTest, workingPath) {
+	EXPECT_EQ("data/", fs.workingPath("data/file.extension"));
+	EXPECT_EQ("data/subfolder/", fs.workingPath("data/subfolder/file.extension"));
+	EXPECT_EQ("anotherFolder/", fs.workingPath("anotherFolder/file.extension"));
+	EXPECT_EQ("", fs.workingPath("file.extension"));
+}

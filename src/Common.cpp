069d463ad63e37f3b7c2d5b0fab3224deae73c24
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-05-20 16:15:33 -0600

    Use `istringstream` in `split`

--- src/Common.cpp
+++ src/Common.cpp
@@ -159,11 +159,7 @@ std::vector<std::string> NAS2D::split(std::string str, char delim /*= ','*/, boo
 	NAS2D::StringList result{};
 	result.reserve(potential_count);
 
-	std::stringstream ss{};
-	ss.str(str);
-	ss.seekg(0);
-	ss.seekp(0);
-	ss.clear();
+	std::istringstream ss(str);
 
 	std::string curString{};
 	while (std::getline(ss, curString, delim))

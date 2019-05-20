3fb09269bd6ff80c8164e62e7ad63c9793c3a9a8
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-05-20 15:29:17 -0600

    Add `split` unit tests

--- test/Common.test.cpp
+++ test/Common.test.cpp
@@ -6,3 +6,26 @@
 TEST(Version, versionString) {
 	EXPECT_THAT(NAS2D::versionString(), testing::MatchesRegex("[[:digit:]]+\\.[[:digit:]]+\\.[[:digit:]]+"));
 }
+
+TEST(String, split) {
+	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::split("a,b,c"));
+	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::split("abc"));
+	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::split(",abc"));
+	EXPECT_EQ((NAS2D::StringList{"a", "bc"}), NAS2D::split("a,bc"));
+	EXPECT_EQ((NAS2D::StringList{"ab", "c"}), NAS2D::split("ab,c"));
+	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::split("abc,"));
+
+	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::split("a.b.c", '.'));
+	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::split("abc", '.'));
+	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::split(".abc", '.'));
+	EXPECT_EQ((NAS2D::StringList{"a", "bc"}), NAS2D::split("a.bc", '.'));
+	EXPECT_EQ((NAS2D::StringList{"ab", "c"}), NAS2D::split("ab.c", '.'));
+	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::split("abc.", '.'));
+
+	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::split("a,b,c", ',', false));
+	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::split("abc", ',', false));
+	EXPECT_EQ((NAS2D::StringList{"", "abc"}), NAS2D::split(",abc", ',', false));
+	EXPECT_EQ((NAS2D::StringList{"a", "bc"}), NAS2D::split("a,bc", ',', false));
+	EXPECT_EQ((NAS2D::StringList{"ab", "c"}), NAS2D::split("ab,c", ',', false));
+	// EXPECT_EQ((NAS2D::StringList{"abc", ""}), NAS2D::split("abc,", ',', false));  // Actual: {"abc"}
+}

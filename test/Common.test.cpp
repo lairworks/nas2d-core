f26fef097921180ba749faa7aa20538b35e9989f
cugone <cugone@gmail.com>
2019-05-22 12:49:36 -0500

    Swapped order of tests

--- test/Common.test.cpp
+++ test/Common.test.cpp
@@ -8,6 +8,13 @@ TEST(Version, versionString) {
 }
 
 TEST(String, split) {
+	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::split("a,b,c", ','));
+	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::split("abc", ','));
+	EXPECT_EQ((NAS2D::StringList{"", "abc"}), NAS2D::split(",abc", ','));
+	EXPECT_EQ((NAS2D::StringList{"a", "bc"}), NAS2D::split("a,bc", ','));
+	EXPECT_EQ((NAS2D::StringList{"ab", "c"}), NAS2D::split("ab,c", ','));
+	// EXPECT_EQ((NAS2D::StringList{"abc", ""}), NAS2D::split("abc,", ','));  // Actual: {"abc"}
+
 	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::splitSkipEmpty("a,b,c"));
 	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitSkipEmpty("abc"));
 	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitSkipEmpty(",abc"));
@@ -22,10 +29,4 @@ TEST(String, split) {
 	EXPECT_EQ((NAS2D::StringList{"ab", "c"}), NAS2D::splitSkipEmpty("ab.c", '.'));
 	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitSkipEmpty("abc.", '.'));
 
-	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::split("a,b,c", ','));
-	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::split("abc", ','));
-	EXPECT_EQ((NAS2D::StringList{"", "abc"}), NAS2D::split(",abc", ','));
-	EXPECT_EQ((NAS2D::StringList{"a", "bc"}), NAS2D::split("a,bc", ','));
-	EXPECT_EQ((NAS2D::StringList{"ab", "c"}), NAS2D::split("ab,c", ','));
-	// EXPECT_EQ((NAS2D::StringList{"abc", ""}), NAS2D::split("abc,", ','));  // Actual: {"abc"}
 }

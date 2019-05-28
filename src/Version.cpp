b2b7758dcb26d8722658fde983e8250e25ac78ea
cugone <cugone@gmail.com>
2019-05-28 09:55:10 -0500

    Implement Version.h/.cpp
    Declare version constants to be constexpr;
    Declare functions to be noexcept;
    Declare versionString to return a const std::string

---
+++ src/Version.cpp
@@ -0,0 +1,40 @@
+// ==================================================================================
+// = NAS2D
+// = Copyright © 2008 - 2019 New Age Software
+// ==================================================================================
+// = NAS2D is distributed under the terms of the zlib license. You are free to copy,
+// = modify and distribute the software under the terms of the zlib license.
+// =
+// = Acknowledgment of your use of NAS2D is appreciated but is not required.
+// ==================================================================================
+
+#include "NAS2D/Version.h"
+
+#include <sstream>
+
+namespace NAS2D
+{
+
+	const std::string NAS2D::versionString() noexcept
+	{
+		std::ostringstream ss;
+		ss << versionMajor() << "." << versionMinor() << "." << versionPatch();
+		return ss.str();
+	}
+
+	constexpr int NAS2D::versionMajor() noexcept
+	{
+		return NAS2D_MAJOR_VERSION;
+	}
+
+	constexpr int NAS2D::versionMinor() noexcept
+	{
+		return NAS2D_MINOR_VERSION;
+	}
+
+	constexpr int NAS2D::versionPatch() noexcept
+	{
+		return NAS2D_PATCH_VERSION;
+	}
+
+} // namespace NAS2D

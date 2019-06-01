ec05be87a8850d596f22963ef4d2c6e72f554049
cugone <cugone@gmail.com>
2019-06-01 11:18:35 -0500

    Removed bad tests

--- test/Common.test.cpp
+++ test/Common.test.cpp
@@ -29,56 +29,6 @@ TEST(String, split)
 	EXPECT_EQ((NAS2D::StringList{"abc", ""}), NAS2D::split("abc.", '.'));
 }
 
-TEST(String, splitOnUnquoted)
-{
-	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::splitOnUnquoted("a,b,c"));
-	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitOnUnquoted("abc"));
-	EXPECT_EQ((NAS2D::StringList{"", "abc"}), NAS2D::splitOnUnquoted(",abc"));
-	EXPECT_EQ((NAS2D::StringList{"a", "bc"}), NAS2D::splitOnUnquoted("a,bc"));
-	EXPECT_EQ((NAS2D::StringList{"ab", "c"}), NAS2D::splitOnUnquoted("ab,c"));
-	EXPECT_EQ((NAS2D::StringList{"abc", ""}), NAS2D::splitOnUnquoted("abc,"));
-	EXPECT_EQ((NAS2D::StringList{"a,b,c", "abc", "def"}), NAS2D::splitOnUnquoted(R"("a,b,c",abc,def)"));
-	EXPECT_EQ((NAS2D::StringList{"abc", "a,b,c", "def"}), NAS2D::splitOnUnquoted(R"(abc,"a,b,c",def)"));
-	EXPECT_EQ((NAS2D::StringList{"abc", "def", "a,b,c"}), NAS2D::splitOnUnquoted(R"(abc,def,"a,b,c")"));
-	EXPECT_EQ((NAS2D::StringList{"","abc", "def"}), NAS2D::splitOnUnquoted(R"("",abc,def)"));
-	EXPECT_EQ((NAS2D::StringList{"abc", "", "def"}), NAS2D::splitOnUnquoted(R"(abc,"",def)"));
-	EXPECT_EQ((NAS2D::StringList{"abc", "def", ""}), NAS2D::splitOnUnquoted(R"(abc,def,"")"));
-
-	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::splitOnUnquoted("a.b.c", '.'));
-	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitOnUnquoted("abc", '.'));
-	EXPECT_EQ((NAS2D::StringList{"", "abc"}), NAS2D::splitOnUnquoted(".abc", '.'));
-	EXPECT_EQ((NAS2D::StringList{"a", "bc"}), NAS2D::splitOnUnquoted("a.bc", '.'));
-	EXPECT_EQ((NAS2D::StringList{"ab", "c"}), NAS2D::splitOnUnquoted("ab.c", '.'));
-	EXPECT_EQ((NAS2D::StringList{"abc", ""}), NAS2D::splitOnUnquoted("abc.", '.'));
-	EXPECT_EQ((NAS2D::StringList{"a.b.c", "abc", "def"}), NAS2D::splitOnUnquoted(R"("a.b.c".abc.def)", '.'));
-	EXPECT_EQ((NAS2D::StringList{"abc", "a.b.c", "def"}), NAS2D::splitOnUnquoted(R"(abc."a.b.c".def)", '.'));
-	EXPECT_EQ((NAS2D::StringList{"abc", "def", "a.b.c"}), NAS2D::splitOnUnquoted(R"(abc.def."a.b.c")", '.'));
-	EXPECT_EQ((NAS2D::StringList{"", "abc", "def"}), NAS2D::splitOnUnquoted(R"("".abc.def)", '.'));
-	EXPECT_EQ((NAS2D::StringList{"abc", "", "def"}), NAS2D::splitOnUnquoted(R"(abc."".def)", '.'));
-	EXPECT_EQ((NAS2D::StringList{"abc", "def", ""}), NAS2D::splitOnUnquoted(R"(abc.def."")", '.'));
-	EXPECT_EQ((NAS2D::StringList{"Hello", "Hello World", "World"}), NAS2D::splitOnUnquoted(R"(Hello "Hello World" World)", ' '));
-	EXPECT_EQ((NAS2D::StringList{"Hello", " ", "World"}), NAS2D::splitOnUnquoted(R"(Hello " " World)", ' '));
-	EXPECT_EQ((NAS2D::StringList{"Hello", "", "World"}), NAS2D::splitOnUnquoted(R"(Hello "" World)", ' '));
-
-}
-
-TEST(String, splitSkipEmpty)
-{
-	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::splitSkipEmpty("a,b,c"));
-	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitSkipEmpty("abc"));
-	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitSkipEmpty(",abc"));
-	EXPECT_EQ((NAS2D::StringList{"a", "bc"}), NAS2D::splitSkipEmpty("a,bc"));
-	EXPECT_EQ((NAS2D::StringList{"ab", "c"}), NAS2D::splitSkipEmpty("ab,c"));
-	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitSkipEmpty("abc,"));
-
-	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::splitSkipEmpty("a.b.c", '.'));
-	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitSkipEmpty("abc", '.'));
-	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitSkipEmpty(".abc", '.'));
-	EXPECT_EQ((NAS2D::StringList{"a", "bc"}), NAS2D::splitSkipEmpty("a.bc", '.'));
-	EXPECT_EQ((NAS2D::StringList{"ab", "c"}), NAS2D::splitSkipEmpty("ab.c", '.'));
-	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::splitSkipEmpty("abc.", '.'));
-}
-
 TEST(String, SplitOnUnquotedSkipEmpty)
 {
 	using StringList = std::vector<std::string>;

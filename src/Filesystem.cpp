7801cd62410ec69e45b8a661c026840d8c88cc86
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-12-22 05:14:37 -0700

    Remove default value comment

--- src/Filesystem.cpp
+++ src/Filesystem.cpp
@@ -128,7 +128,7 @@ StringList Filesystem::searchPath() const
  *
  * \note	This function will also return the names of any directories in a specified search path
  */
-StringList Filesystem::directoryList(const std::string& dir, const std::string& filter /*= std::string{}*/) const
+StringList Filesystem::directoryList(const std::string& dir, const std::string& filter) const
 {
 	if (!PHYSFS_isInit()) { throw filesystem_not_initialized(); }
 

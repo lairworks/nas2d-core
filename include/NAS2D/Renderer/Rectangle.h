0d1af6d9f45dcbe8f6798e76ed9ffc33668fd80c
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-01-13 10:56:34 -0700

    Replace Rectangle implementations with template using alias
    Use explicit template instantiation in implementation file. This allows
    the compiler to generate code with that type parameter only once.

--- include/NAS2D/Renderer/Rectangle.h
+++ include/NAS2D/Renderer/Rectangle.h
@@ -103,93 +103,19 @@ private:
 };
 
 
-struct Rectangle_2d;
-struct Rectangle_2df;
-
-
 /**
  * \class	Rectangle_2d
  * \brief	2D rectangle.
  */
-struct Rectangle_2d
-{
-public:
-	Rectangle_2d() = default;
-	Rectangle_2d(int x, int y, int w, int h);
-
-public:
-	bool operator==(const Rectangle_2d& rect) const;
-	bool operator!=(const Rectangle_2d& rect) const;
-
-	operator Rectangle_2df() const;
-
-public:
-	bool null();
-
-	void x(int x);
-	int x() const;
-	int& x();
-
-	void y(int y);
-	int y() const;
-	int& y();
-
-	void width(int w);
-	int width() const;
-	int& width();
-
-	void height(int h);
-	int height() const;
-	int& height();
-
-	int center_x() const;
-	int center_y() const;
-
-private:
-	int mX = 0, mY = 0, mW = 0, mH = 0;
-};
+using Rectangle_2d = Rectangle<int>;
+extern template struct Rectangle<int>;
 
 
 /**
  * \class	Rectangle_2df
  * \brief	Floating point 2D Rectangle.
  */
-struct Rectangle_2df
-{
-public:
-	Rectangle_2df() = default;
-	Rectangle_2df(float x, float y, float w, float h);
-
-public:
-	bool operator==(const Rectangle_2df& rect) const;
-	bool operator!=(const Rectangle_2df& rect) const;
-
-	operator Rectangle_2d() const;
-
-public:
-	bool null();
-
-	void x(float x);
-	float x() const;
-	float& x();
-
-	void y(float y);
-	float y() const;
-	float& y();
-
-	void width(float w);
-	float width() const;
-	float& width();
-
-	void height(float h);
-	float height() const;
-	float& height();
-
-	float center_x() const;
-	float center_y() const;
-
-private:
-	float mX = 0.0f, mY = 0.0f, mW = 0.0f, mH = 0.0f;
-};
+using Rectangle_2df = Rectangle<float>;
+extern template struct Rectangle<float>;
 
 } // namespace

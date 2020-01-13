bcd4dbc84521fb268f537593221ff1a36affa096
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-01-13 10:43:58 -0700

    Create template `Rectangle` struct

--- include/NAS2D/Renderer/Rectangle.h
+++ include/NAS2D/Renderer/Rectangle.h
@@ -13,6 +13,96 @@
 namespace NAS2D {
 
 
+template <typename BaseType>
+struct Rectangle
+{
+	Rectangle() = default;
+	Rectangle(const Rectangle& other) = default;
+	Rectangle& operator=(const Rectangle& other) = default;
+
+	Rectangle(BaseType x, BaseType y, BaseType width, BaseType height) :
+		mX(x),
+		mY(y),
+		mW(width),
+		mH(height)
+	{}
+
+	bool operator==(const Rectangle& rect) const {
+		return (mX == rect.mX) && (mY == rect.mY) && (mW == rect.mW) && (mH == rect.mH);
+	}
+	bool operator!=(const Rectangle& rect) const {
+		return !(*this == rect);
+	}
+
+	bool null() const {
+		return (mW == 0) || (mH == 0);
+	}
+
+	template <typename NewBaseType>
+	operator Rectangle<NewBaseType>() const {
+		return {
+			static_cast<NewBaseType>(mX),
+			static_cast<NewBaseType>(mY),
+			static_cast<NewBaseType>(mW),
+			static_cast<NewBaseType>(mH)
+		};
+	}
+
+	void x(BaseType x) {
+		mX = x;
+	}
+	BaseType x() const {
+		return mX;
+	}
+	BaseType& x() {
+		return mX;
+	}
+
+	void y(BaseType y) {
+		mY = y;
+	}
+	BaseType y() const {
+		return mY;
+	}
+	BaseType& y() {
+		return mY;
+	}
+
+	void width(BaseType w) {
+		mW = w;
+	}
+	BaseType width() const {
+		return mW;
+	}
+	BaseType& width() {
+		return mW;
+	}
+
+	void height(BaseType h) {
+		mH = h;
+	}
+	BaseType height() const {
+		return mH;
+	}
+	BaseType& height() {
+		return mH;
+	}
+
+	BaseType center_x() const {
+		return mX + (mW / 2);
+	}
+	BaseType center_y() const {
+		return mY + (mH / 2);
+	}
+
+private:
+	BaseType mX = 0;
+	BaseType mY = 0;
+	BaseType mW = 0;
+	BaseType mH = 0;
+};
+
+
 struct Rectangle_2d;
 struct Rectangle_2df;
 

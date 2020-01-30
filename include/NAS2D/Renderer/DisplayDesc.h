749b033e3a07dd8a35095aed741116e45b9c08ad
cugone <cugone@gmail.com>
2020-01-30 13:24:17 -0600

    Implement DisplayDesc

---
+++ include/NAS2D/Renderer/DisplayDesc.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace NAS2D
+{
+	//A structure represents a display description.
+	struct DisplayDesc
+	{
+		int width{0};
+		int height{0};
+		int refreshHz{0};
+	};
+
+	bool operator==(const DisplayDesc& a, const DisplayDesc& b);
+	bool operator!=(const DisplayDesc& a, const DisplayDesc& b);
+
+} // namespace NAS2D

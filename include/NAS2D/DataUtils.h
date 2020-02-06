19efd4552ada73734eafbe72eca08a63f5fa5d16
cugone <cugone@gmail.com>
2020-02-06 16:10:26 -0600

    Implement XmlValidation

---
+++ include/NAS2D/DataUtils.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "NAS2D/StringUtils.h"
+
+#include <string>
+
+namespace NAS2D
+{
+	namespace Xml
+	{
+		class XmlElement;
+
+		void validateXMLElement(
+			const XmlElement& element,
+			const std::string& name,
+			const std::string& requiredChildElements,
+			const std::string& requiredAttributes,
+			const std::string& optionalChildElements = std::string{},
+			const std::string& optionalAttributes = std::string{});
+
+	} // namespace Xml
+
+} // namespace DataUtils

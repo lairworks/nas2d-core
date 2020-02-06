ca295e2af38e3d5985fad4ef038b74710d880e27
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-02-06 02:23:53 -0700

    Add unit tests for `basePath` and `prefPath`
    These results vary by platform, so there is no specific value to test
    against. Instead, we want to ensure the result represents a directory.

--- test/Filesystem.test.cpp
+++ test/Filesystem.test.cpp
@@ -17,6 +17,16 @@ class FilesystemTest : public ::testing::Test {
 };
 
 
+TEST_F(FilesystemTest, basePath) {
+	// Result is a path, so should end with a directory separator
+	EXPECT_THAT(fs.basePath(), testing::EndsWith(fs.dirSeparator()));
+}
+
+TEST_F(FilesystemTest, prefPath) {
+	// Result is a path, so should end with a directory separator
+	EXPECT_THAT(fs.prefPath(), testing::EndsWith(fs.dirSeparator()));
+}
+
 TEST_F(FilesystemTest, dataPath) {
 	EXPECT_EQ("data/", fs.dataPath());
 }

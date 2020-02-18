a8d3e75b0ec7e8d014b2ba3d07dace8c772b5d86
cugone <cugone@gmail.com>
2020-02-18 12:13:17 -0600

    Phase 1 of value_helper simplification

--- src/DataUtils.cpp
+++ src/DataUtils.cpp
@@ -6,16 +6,7 @@
 
 bool NAS2D::Dictionary::value_helper(const std::string& key, bool) const
 {
-	auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
+	auto valueAsStr = value<std::string>(key);
 	valueAsStr = NAS2D::trimWhitespace(NAS2D::toLowercase(valueAsStr));
 	if (valueAsStr == "true" || valueAsStr == "false")
 	{
@@ -23,415 +14,105 @@ bool NAS2D::Dictionary::value_helper(const std::string& key, bool) const
 	}
 	else
 	{
-		const auto valueAsBool = [&valueAsStr]() {
-			try
-			{
-				return std::stoi(valueAsStr) != 0;
-			}
-			catch (const std::invalid_argument&)
-			{
-				const std::string err_msg{"Dictionary value \'" + valueAsStr + "\' is not convertible to 'integer'"};
-				throw std::runtime_error(err_msg);
-			}
-			catch (const std::out_of_range&)
-			{
-				const std::string err_msg{"Dictionary value \'" + valueAsStr + "\' is out of range of 'integer'."};
-				throw std::runtime_error(err_msg);
-			}
-		}(); // IILE
+		const auto valueAsBool = std::stoi(valueAsStr) != 0;
 		return valueAsBool;
 	}
 }
 
 char NAS2D::Dictionary::value_helper(const std::string& key, char) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
 	return valueAsStr.at(0);
 }
 
 signed char NAS2D::Dictionary::value_helper(const std::string& key, signed char) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsSChar = [&valueAsStr]() {
-		try
-		{
-			return static_cast<signed char>(std::stoi(valueAsStr));
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsSChar = static_cast<signed char>(std::stoi(valueAsStr));
 	return valueAsSChar;
 }
 
 unsigned char NAS2D::Dictionary::value_helper(const std::string& key, unsigned char) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsUChar = [&valueAsStr]() {
-		try
-		{
-			return static_cast<unsigned char>(std::stoul(valueAsStr));
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsUChar = static_cast<unsigned char>(std::stoul(valueAsStr));
 	return valueAsUChar;
 }
 
 short NAS2D::Dictionary::value_helper(const std::string& key, short) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsShort = [&valueAsStr]() {
-		try
-		{
-			return static_cast<short>(std::stoi(valueAsStr));
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsShort = static_cast<short>(std::stoi(valueAsStr));
 	return valueAsShort;
 }
 
 unsigned short NAS2D::Dictionary::value_helper(const std::string& key, unsigned short) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsUShort = [&valueAsStr]() {
-		try
-		{
-			return static_cast<unsigned short>(std::stoul(valueAsStr));
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsUShort = static_cast<unsigned short>(std::stoul(valueAsStr));
 	return valueAsUShort;
 }
 
 int NAS2D::Dictionary::value_helper(const std::string& key, int) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsInt = [&valueAsStr]() {
-		try
-		{
-			return std::stoi(valueAsStr);
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsInt = std::stoi(valueAsStr);
 	return valueAsInt;
 }
 
 unsigned int NAS2D::Dictionary::value_helper(const std::string& key, unsigned int) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsUInt = [&valueAsStr]() {
-		try
-		{
-			return static_cast<unsigned int>(std::stoul(valueAsStr));
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsUInt = static_cast<unsigned int>(std::stoul(valueAsStr));
 	return valueAsUInt;
 }
 
 long NAS2D::Dictionary::value_helper(const std::string& key, long) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsLong = [&valueAsStr]() {
-		try
-		{
-			return std::stol(valueAsStr);
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsLong = std::stol(valueAsStr);
 	return valueAsLong;
 }
 
 unsigned long NAS2D::Dictionary::value_helper(const std::string& key, unsigned long) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsULong = [&valueAsStr]() {
-		try
-		{
-			return std::stoul(valueAsStr);
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsULong = std::stoul(valueAsStr);
 	return valueAsULong;
 }
 
 long long NAS2D::Dictionary::value_helper(const std::string& key, long long) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsLongLong = [&valueAsStr]() {
-		try
-		{
-			return std::stoll(valueAsStr);
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsLongLong = std::stoll(valueAsStr);
 	return valueAsLongLong;
 }
 
 unsigned long long NAS2D::Dictionary::value_helper(const std::string& key, unsigned long long) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsULongLong = [&valueAsStr]() {
-		try
-		{
-			return std::stoull(valueAsStr);
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsULongLong = std::stoull(valueAsStr);
 	return valueAsULongLong;
 }
 
 float NAS2D::Dictionary::value_helper(const std::string& key, float) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsFloat = [&valueAsStr]() {
-		try
-		{
-			return std::stof(valueAsStr);
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsFloat = std::stof(valueAsStr);
 	return valueAsFloat;
 }
 
 double NAS2D::Dictionary::value_helper(const std::string& key, double) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsDouble = [&valueAsStr]() {
-		try
-		{
-			return std::stod(valueAsStr);
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsDouble = std::stod(valueAsStr);
 	return valueAsDouble;
 }
 
 long double NAS2D::Dictionary::value_helper(const std::string& key, long double) const
 {
-	const auto valueAsStr = [this, &key]() -> std::string {
-		try
-		{
-			return value<std::string>(key);
-		}
-		catch (...)
-		{
-			throw;
-		}
-	}(); // IILE
-	const auto valueAsLongDouble = [&valueAsStr]() {
-		try
-		{
-			return std::stold(valueAsStr);
-		}
-		catch (const std::invalid_argument&)
-		{
-			throw;
-		}
-		catch (const std::out_of_range&)
-		{
-			throw;
-		}
-	}(); // IILE
+	const auto valueAsStr = value<std::string>(key);
+	const auto valueAsLongDouble = std::stold(valueAsStr);
 	return valueAsLongDouble;
 }
 

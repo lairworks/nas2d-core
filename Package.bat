2dec832f3ff8a5d3e6c696e418c406de29599f92
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-04-13 00:12:04 -0600

    Create batch file to package Windows release

---
+++ Package.bat
@@ -0,0 +1,23 @@
+@echo off
+
+set ProjectName=NAS2D
+set OutputFolder=Temporary\
+set PackageFolder=%OutputFolder%Package\
+
+set TargetPath=%1
+set Platform=%2
+set Configuration=%3
+
+
+for /f %%i in ('git describe --tags --dirty') do set Version=%%i
+
+set Config=Windows.%Platform%.%Configuration%
+set PackageName=nas2d-%Version%-%Config%.zip
+
+cd /D "%~dp0"
+xcopy /y /q "%TargetPath%" "%PackageFolder%"
+xcopy /s /y /q NAS2D\*.h "%PackageFolder%include\"
+cd "%PackageFolder%"
+
+powershell -Command "Compress-Archive -Path 'NAS2D.lib', 'include/' -DestinationPath '%PackageName%' -Force"
+move /Y *.zip ..

1eca028370deb0aa1837a1ec2e6084b339ed8131
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-04-15 20:58:38 -0600

    Change loop variable to int to match other values

--- NAS2D/Resources/Font.cpp
+++ NAS2D/Resources/Font.cpp
@@ -311,9 +311,9 @@ bool loadBitmap(const std::string& path, int glyphWidth, int glyphHeight, int gl
 		glm[i].minX = glyphWidth;
 	}
 
-	for (std::size_t row = 0; row < GLYPH_MATRIX_SIZE; row++)
+	for (int row = 0; row < GLYPH_MATRIX_SIZE; row++)
 	{
-		for (std::size_t col = 0; col < GLYPH_MATRIX_SIZE; col++)
+		for (int col = 0; col < GLYPH_MATRIX_SIZE; col++)
 		{
 			const std::size_t glyph = (row * GLYPH_MATRIX_SIZE) + col;
 

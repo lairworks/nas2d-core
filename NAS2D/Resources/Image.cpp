3e97b767b209ef35ffa75a467a790f60ad45af5a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-04-16 17:58:04 -0600

    Move file local variable and constants into unnamed namespace

--- NAS2D/Resources/Image.cpp
+++ NAS2D/Resources/Image.cpp
@@ -24,19 +24,19 @@
 using namespace NAS2D;
 using namespace NAS2D::Exception;
 
-const std::string DEFAULT_IMAGE_NAME	= "Default Image";
-const std::string ARBITRARY_IMAGE_NAME	= "arbitrary_image_";
-
 
 using TextureIdMap = std::map<std::string, ImageInfo>;
-
 TextureIdMap imageIdMap; /**< Lookup table for OpenGL Texture ID's. */
-int IMAGE_ARBITRARY = 0; /**< Counter for arbitrary image ID's. */
+
 
 // ==================================================================================
 // = UNEXPOSED FUNCTION PROTOTYPES
 // ==================================================================================
 namespace {
+const std::string DEFAULT_IMAGE_NAME	= "Default Image";
+const std::string ARBITRARY_IMAGE_NAME	= "arbitrary_image_";
+int IMAGE_ARBITRARY = 0; /**< Counter for arbitrary image ID's. */
+
 bool checkTextureId(const std::string& name);
 unsigned int generateTexture(void *buffer, int bytesPerPixel, int width, int height);
 void updateImageReferenceCount(const std::string& name);

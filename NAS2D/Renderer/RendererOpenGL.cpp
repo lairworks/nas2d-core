550513298fb6c702e2166efbe44baa53407822eb
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-04-17 20:22:06 -0600

    Use Image.size() in generate_fbo

--- NAS2D/Renderer/RendererOpenGL.cpp
+++ NAS2D/Renderer/RendererOpenGL.cpp
@@ -831,7 +831,8 @@ GLuint generate_fbo(Image& image)
 		glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
 		const auto textureFormat = (SDL_BYTEORDER == SDL_BIG_ENDIAN) ? GL_BGRA : GL_RGBA;
 
-		glTexImage2D(GL_TEXTURE_2D, 0, textureFormat, image.width(), image.height(), 0, textureFormat, GL_UNSIGNED_BYTE, nullptr);
+		const auto imageSize = image.size();
+		glTexImage2D(GL_TEXTURE_2D, 0, textureFormat, imageSize.x, imageSize.y, 0, textureFormat, GL_UNSIGNED_BYTE, nullptr);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	}

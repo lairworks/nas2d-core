a8cc585db87da24167110c45f1c7673e9501be37
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-04-23 04:07:37 -0600

    Add unit tests for stringTo template method

--- test/StringUtils.test.cpp
+++ test/StringUtils.test.cpp
@@ -41,6 +41,52 @@ TEST(String, stringFrom) {
 	EXPECT_THAT(NAS2D::stringFrom(0.0l), testing::StartsWith("0.0"));
 }
 
+TEST(String, stringTo) {
+	EXPECT_EQ("SomeStringValue", NAS2D::stringTo<std::string>("SomeStringValue"));
+	EXPECT_EQ("SomeStringValue", NAS2D::stringTo<std::string>(std::string{"SomeStringValue"}));
+
+	EXPECT_THROW(NAS2D::stringTo<bool>(""), std::invalid_argument);
+	EXPECT_THROW(NAS2D::stringTo<bool>("random text"), std::invalid_argument);
+	EXPECT_EQ(false, NAS2D::stringTo<bool>("false"));
+	EXPECT_EQ(true, NAS2D::stringTo<bool>("true"));
+
+	EXPECT_THROW(NAS2D::stringTo<char>(std::to_string(std::numeric_limits<char>::min() - 1)), std::out_of_range);
+	EXPECT_THROW(NAS2D::stringTo<char>(std::to_string(std::numeric_limits<char>::max() + 1)), std::out_of_range);
+	EXPECT_EQ(char{0}, NAS2D::stringTo<char>("0"));
+	using signedChar = signed char;
+	EXPECT_EQ(signedChar{-1}, NAS2D::stringTo<signedChar>("-1"));
+	EXPECT_EQ(signedChar{0}, NAS2D::stringTo<signedChar>("0"));
+	using unsignedChar = unsigned char;
+	EXPECT_EQ(unsignedChar{0}, NAS2D::stringTo<unsignedChar>("0"));
+
+	EXPECT_THROW(NAS2D::stringTo<short>(std::to_string(std::numeric_limits<short>::min() - 1)), std::out_of_range);
+	EXPECT_THROW(NAS2D::stringTo<short>(std::to_string(std::numeric_limits<short>::max() + 1)), std::out_of_range);
+	EXPECT_EQ(short{-1}, NAS2D::stringTo<short>("-1"));
+	EXPECT_EQ(short{0}, NAS2D::stringTo<short>("0"));
+	using unsignedShort = unsigned short;
+	EXPECT_EQ(unsignedShort{0}, NAS2D::stringTo<unsignedShort>("0"));
+
+	EXPECT_EQ(int{-1}, NAS2D::stringTo<int>("-1"));
+	EXPECT_EQ(int{0}, NAS2D::stringTo<int>("0"));
+	using unsignedInt = unsigned int;
+	EXPECT_EQ(unsignedInt{0}, NAS2D::stringTo<unsignedInt>("0"));
+
+	EXPECT_EQ(long{-1}, NAS2D::stringTo<long>("-1"));
+	EXPECT_EQ(long{0}, NAS2D::stringTo<long>("0"));
+	using unsignedLong = unsigned long;
+	EXPECT_EQ(unsignedLong{0}, NAS2D::stringTo<unsignedLong>("0"));
+
+	using longLong = long long;
+	EXPECT_EQ(longLong{-1}, NAS2D::stringTo<longLong>("-1"));
+	EXPECT_EQ(longLong{0}, NAS2D::stringTo<longLong>("0"));
+	using unsignedLongLong = unsigned long long;
+	EXPECT_EQ(unsignedLongLong{0}, NAS2D::stringTo<unsignedLongLong>("0"));
+
+	EXPECT_EQ(0.0f, NAS2D::stringTo<float>("0.0"));
+	EXPECT_EQ(0.0, NAS2D::stringTo<double>("0.0"));
+	EXPECT_EQ(0.0l, NAS2D::stringTo<long double>("0.0"));
+}
+
 TEST(String, split) {
 	EXPECT_EQ((NAS2D::StringList{"a", "b", "c"}), NAS2D::split("a,b,c"));
 	EXPECT_EQ((NAS2D::StringList{"abc"}), NAS2D::split("abc"));

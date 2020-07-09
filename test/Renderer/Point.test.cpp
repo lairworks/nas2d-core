8ab039819de0b5d23ad1e64f8338234833b0d2ac
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-07-08 21:11:31 -0600

    Use Point deduction guide in Point unit tests
    Maintain explicit type for the type conversion tests.
    
    Maintain explicit type in addition and subtraction expression tests. It
    seems GCC complains when the type is omitted there, even though other
    compilers can handle it.

--- test/Renderer/Point.test.cpp
+++ test/Renderer/Point.test.cpp
@@ -3,39 +3,39 @@
 
 
 TEST(Point, DefaultConstructibleZeroInit) {
-	EXPECT_EQ((NAS2D::Point<int>{0, 0}), NAS2D::Point<int>{});
+	EXPECT_EQ((NAS2D::Point{0, 0}), NAS2D::Point<int>{});
 }
 
 TEST(Point, OperatorEqualNotEqual) {
-	EXPECT_EQ((NAS2D::Point<int>{1, 1}), (NAS2D::Point<int>{1, 1}));
-	EXPECT_EQ((NAS2D::Point<int>{2, 2}), (NAS2D::Point<int>{2, 2}));
+	EXPECT_EQ((NAS2D::Point{1, 1}), (NAS2D::Point{1, 1}));
+	EXPECT_EQ((NAS2D::Point{2, 2}), (NAS2D::Point{2, 2}));
 
-	EXPECT_NE((NAS2D::Point<int>{1, 1}), (NAS2D::Point<int>{1, 2}));
-	EXPECT_NE((NAS2D::Point<int>{1, 1}), (NAS2D::Point<int>{2, 1}));
+	EXPECT_NE((NAS2D::Point{1, 1}), (NAS2D::Point{1, 2}));
+	EXPECT_NE((NAS2D::Point{1, 1}), (NAS2D::Point{2, 1}));
 }
 
 TEST(Point, SelfAddVector) {
-	NAS2D::Point<int> point{1, 1};
+	NAS2D::Point point{1, 1};
 	EXPECT_EQ(&point, &(point += NAS2D::Vector{1, 2}));
-	EXPECT_EQ((NAS2D::Point<int>{2, 3}), point);
+	EXPECT_EQ((NAS2D::Point{2, 3}), point);
 }
 
 TEST(Point, SelfSubtractVector) {
-	NAS2D::Point<int> point{2, 5};
+	NAS2D::Point point{2, 5};
 	EXPECT_EQ(&point, &(point -= NAS2D::Vector{1, 3}));
-	EXPECT_EQ((NAS2D::Point<int>{1, 2}), point);
+	EXPECT_EQ((NAS2D::Point{1, 2}), point);
 }
 
 TEST(Vector, AddVector) {
-	EXPECT_EQ((NAS2D::Point<int>{2, 3}), (NAS2D::Point<int>{1, 1}) + (NAS2D::Vector{1, 2}));
+	EXPECT_EQ((NAS2D::Point{2, 3}), (NAS2D::Point<int>{1, 1}) + (NAS2D::Vector{1, 2}));
 }
 
 TEST(Vector, SubtractVector) {
-	EXPECT_EQ((NAS2D::Point<int>{1, 2}), (NAS2D::Point<int>{2, 5}) - (NAS2D::Vector{1, 3}));
+	EXPECT_EQ((NAS2D::Point{1, 2}), (NAS2D::Point<int>{2, 5}) - (NAS2D::Vector{1, 3}));
 }
 
 TEST(Vector, SubtractPointToVector) {
-	EXPECT_EQ((NAS2D::Vector{1, 1}), (NAS2D::Point<int>{2, 3}) - (NAS2D::Point<int>{1, 2}));
+	EXPECT_EQ((NAS2D::Vector{1, 1}), (NAS2D::Point<int>{2, 3}) - (NAS2D::Point{1, 2}));
 }
 
 TEST(Point, OperatorType) {

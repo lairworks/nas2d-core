d649525a42f97db35638b3b73ae2e9eadd54f53c
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-07-19 09:37:03 -0600

    Add Color::alphaFade unit tests

--- test/Renderer/Color.test.cpp
+++ test/Renderer/Color.test.cpp
@@ -13,3 +13,21 @@ TEST(Color, OperatorCompare) {
 	EXPECT_NE((NAS2D::Color{0, 0, 1, 0}), (NAS2D::Color{0, 0, 0, 0}));
 	EXPECT_NE((NAS2D::Color{0, 0, 0, 1}), (NAS2D::Color{0, 0, 0, 0}));
 }
+
+TEST(Color, alphaFade) {
+	EXPECT_EQ((NAS2D::Color{255, 255, 255, 255}), NAS2D::Color::White.alphaFade(255));
+	EXPECT_EQ((NAS2D::Color{255, 255, 255, 128}), NAS2D::Color::White.alphaFade(128));
+	EXPECT_EQ((NAS2D::Color{255, 255, 255, 0}), NAS2D::Color::White.alphaFade(0));
+
+	EXPECT_EQ((NAS2D::Color{255, 0, 0, 255}), NAS2D::Color::Red.alphaFade(255));
+	EXPECT_EQ((NAS2D::Color{255, 0, 0, 128}), NAS2D::Color::Red.alphaFade(128));
+	EXPECT_EQ((NAS2D::Color{255, 0, 0, 0}), NAS2D::Color::Red.alphaFade(0));
+
+	EXPECT_EQ((NAS2D::Color{0, 255, 0, 255}), NAS2D::Color::Green.alphaFade(255));
+	EXPECT_EQ((NAS2D::Color{0, 255, 0, 128}), NAS2D::Color::Green.alphaFade(128));
+	EXPECT_EQ((NAS2D::Color{0, 255, 0, 0}), NAS2D::Color::Green.alphaFade(0));
+
+	EXPECT_EQ((NAS2D::Color{0, 0, 255, 255}), NAS2D::Color::Blue.alphaFade(255));
+	EXPECT_EQ((NAS2D::Color{0, 0, 255, 128}), NAS2D::Color::Blue.alphaFade(128));
+	EXPECT_EQ((NAS2D::Color{0, 0, 255, 0}), NAS2D::Color::Blue.alphaFade(0));
+}

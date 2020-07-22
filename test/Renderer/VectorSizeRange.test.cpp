f478bf7fee3d9570f7563a2d5d00bea21e3a7a1d
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-07-22 04:54:34 -0600

    Add unit tests for VectorSizeRange

---
+++ test/Renderer/VectorSizeRange.test.cpp
@@ -0,0 +1,30 @@
+#include "NAS2D/Renderer/VectorSizeRange.h"
+#include <gtest/gtest.h>
+#include <vector>
+
+
+TEST(VectorSizeRange, Iteration) {
+	using Items = std::vector<NAS2D::Vector<int>>;
+
+	const auto vectorRange1 = NAS2D::VectorSizeRange{NAS2D::Vector{0, 0}};
+	const auto vectorRange2 = NAS2D::VectorSizeRange{NAS2D::Vector{1, 1}};
+	const auto vectorRange3 = NAS2D::VectorSizeRange{NAS2D::Vector{2, 3}};
+
+	// Dereference produces the expected starting value
+	// (Later tests also proves the range is restartable)
+	EXPECT_EQ((NAS2D::Vector{0, 0}), *vectorRange1.begin());
+	EXPECT_EQ((NAS2D::Vector{0, 0}), *vectorRange2.begin());
+	EXPECT_EQ((NAS2D::Vector{0, 0}), *vectorRange3.begin());
+
+	// Range-for syntax is supported
+	const auto fillByRangeFor = [](auto vectorRange) {
+		Items collection;
+		for (const auto vector : vectorRange) {
+			collection.push_back(vector);
+		}
+		return collection;
+	};
+	EXPECT_EQ(fillByRangeFor(vectorRange1), (Items{}));
+	EXPECT_EQ(fillByRangeFor(vectorRange2), (Items{{0, 0}}));
+	EXPECT_EQ(fillByRangeFor(vectorRange3), (Items{{0, 0}, {1, 0}, {0, 1}, {1, 1}, {0, 2}, {1, 2}}));
+}

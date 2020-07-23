ccb585ba87b9220697cb9f599ea2a67c4afa31fd
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-07-23 17:42:53 -0600

    Remove setting texture coordinates in initGL
    Every glDrawArrays call either sets `glTexCoordPointer` shortly before,
    or is wrapped by calls to disable texturing (and then re-enable it).

--- NAS2D/Renderer/RendererOpenGL.cpp
+++ NAS2D/Renderer/RendererOpenGL.cpp
@@ -713,8 +713,6 @@ void RendererOpenGL::initGL()
 
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-
-	glTexCoordPointer(2, GL_FLOAT, 0, defaultTextureCoords);
 }
 
 

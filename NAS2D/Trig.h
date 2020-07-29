4193c1d413167093752b29ff862cc02a29589460
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-07-29 15:33:32 -0600

    Remove type overspecification of literal constant

--- NAS2D/Trig.h
+++ NAS2D/Trig.h
@@ -17,7 +17,7 @@ namespace NAS2D {
 constexpr float PI = 3.14159265f;
 constexpr float PI_2 = PI * 2;
 
-constexpr float DEG2RAD = PI / 180.0f;
+constexpr float DEG2RAD = PI / 180;
 constexpr float RAD2DEG = 180 / PI;
 
 

5518baf35befbbf06fccbeeb1ed0cf86ac75a45e
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-08-01 02:28:45 -0600

    Add unit teste for ResourceCache class

---
+++ test/Resources/ResourceCache.test.cpp
@@ -0,0 +1,41 @@
+#include "NAS2D/Resources/ResourceCache.h"
+#include <gtest/gtest.h>
+
+
+TEST(ResourceCache, load) {
+	class MockResource {
+	public:
+		MockResource(const std::string& initString, int initValue) :
+			string{initString},
+			value{initValue}
+		{}
+
+		bool operator==(const MockResource& other) const {
+			return string == other.string && value == other.value;
+		}
+	private:
+		std::string string;
+		int value;
+	};
+
+	ResourceCache<MockResource, std::string, int> cache;
+
+	const auto& value1 = cache.load("abc", 123);
+	const auto& value2 = cache.load("abc", 123);
+	const auto& value3 = cache.load("abcd", 123);
+	const auto& value4 = cache.load("abc", 1234);
+
+	// Ensure expected values were returned
+	EXPECT_EQ((MockResource{"abc", 123}), value1);
+	EXPECT_EQ((MockResource{"abc", 123}), value2);
+	EXPECT_EQ((MockResource{"abcd", 123}), value3);
+	EXPECT_EQ((MockResource{"abc", 1234}), value4);
+
+	// Ensure expected identity of objects (based on their address)
+	// References to objects with the same load parameters should be the same object
+	// References to objects with different load parameters should be different
+	EXPECT_EQ(&value1, &value2);
+	EXPECT_NE(&value1, &value3);
+	EXPECT_NE(&value1, &value4);
+	EXPECT_NE(&value3, &value4);
+}

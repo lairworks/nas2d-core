2e6aa49dfcb9a001cffd6c569287705d9a9cdc98
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-08-30 04:32:33 -0600

    Add `--recurse` option to vcpkg
    Allows packages to be uninstalled, such as equivalent package with
    different options. This may be needed when options are updated, and a
    developer has a previous installation with a different set of options.

--- InstallVcpkgDeps.bat
+++ InstallVcpkgDeps.bat
@@ -16,4 +16,4 @@ if not defined PLATFORM (
 )
 
 :Install
-vcpkg install --triplet %PLATFORM%-windows  physfs glew SDL2 SDL2-image SDL2-ttf SDL2-mixer[dynamic-load,libflac,libmodplug,libvorbis,mpg123,nativemidi,opusfile] gtest
+vcpkg install --recurse --triplet %PLATFORM%-windows  physfs glew SDL2 SDL2-image SDL2-ttf SDL2-mixer[dynamic-load,libflac,libmodplug,libvorbis,mpg123,nativemidi,opusfile] gtest

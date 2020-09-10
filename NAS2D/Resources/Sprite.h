43cfbee05fd5cbc9c83ae0b27a66528ea3c60fa4
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-09-10 16:11:43 -0600

    Rename SpriteFrame to just Frame

--- NAS2D/Resources/Sprite.h
+++ NAS2D/Resources/Sprite.h
@@ -42,7 +42,7 @@ public:
 	class AnimationSet
 	{
 	public:
-		struct SpriteFrame
+		struct Frame
 		{
 			const Image& image;
 			Rectangle<int> bounds;
@@ -50,15 +50,15 @@ public:
 			unsigned int frameDelay;
 		};
 
-		AnimationSet(std::string fileName, std::map<std::string, Image> imageSheets, std::map<std::string, std::vector<SpriteFrame>> actions);
+		AnimationSet(std::string fileName, std::map<std::string, Image> imageSheets, std::map<std::string, std::vector<Frame>> actions);
 
 		std::vector<std::string> actionNames() const;
-		const std::vector<SpriteFrame>& frames(const std::string& actionName) const;
+		const std::vector<Frame>& frames(const std::string& actionName) const;
 
 	private:
 		std::string mFileName;
 		std::map<std::string, Image> mImageSheets;
-		std::map<std::string, std::vector<SpriteFrame>> mActions;
+		std::map<std::string, std::vector<Frame>> mActions;
 	};
 
 
@@ -98,7 +98,7 @@ private:
 	std::string mSpriteName;
 
 	const AnimationSet* mAnimationSet;
-	const std::vector<AnimationSet::SpriteFrame>* mCurrentAction{nullptr};
+	const std::vector<AnimationSet::Frame>* mCurrentAction{nullptr};
 	std::size_t mCurrentFrame{0};
 
 	bool mPaused{false};

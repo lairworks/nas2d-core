fd47bdc5e1fc19ba0af31b41fb073682263f0527
Brett208 <15183337+Brett208@users.noreply.github.com>
2021-03-17 21:29:58 -0400

    Wrap Xml/XmlSerializer in NAS2D namespace
    Tried to match formatting in rest of code for brace placement on the namespace. It seems contrary to convention in OPHD for brace placement.

--- NAS2D/Xml/XmlSerializer.cpp
+++ NAS2D/Xml/XmlSerializer.cpp
@@ -3,23 +3,23 @@
 #include "../Filesystem.h"
 #include "../Utility.h"
 
-using namespace NAS2D;
-
-Xml::XmlDocument openXmlFile(std::string filename, std::string rootElementName)
-{
-	Xml::XmlDocument xmlDocument;
-	xmlDocument.parse(Utility<Filesystem>::get().open(filename).raw_bytes());
-
-	if (xmlDocument.error())
+namespace NAS2D {
+	Xml::XmlDocument openXmlFile(std::string filename, std::string rootElementName)
 	{
-		throw std::runtime_error(filename + " has malformed XML: Row: " + std::to_string(xmlDocument.errorRow()) + " Column: " + std::to_string(xmlDocument.errorCol()) + " : " + xmlDocument.errorDesc());
-	}
+		Xml::XmlDocument xmlDocument;
+		xmlDocument.parse(Utility<Filesystem>::get().open(filename).raw_bytes());
 
-	const auto* xmlRootElement = xmlDocument.firstChildElement(rootElementName);
-	if (!xmlRootElement)
-	{
-		throw std::runtime_error(filename + " does not contain required root tag of <" + rootElementName + ">");
-	}
+		if (xmlDocument.error())
+		{
+			throw std::runtime_error(filename + " has malformed XML: Row: " + std::to_string(xmlDocument.errorRow()) + " Column: " + std::to_string(xmlDocument.errorCol()) + " : " + xmlDocument.errorDesc());
+		}
 
-	return xmlDocument;
+		const auto* xmlRootElement = xmlDocument.firstChildElement(rootElementName);
+		if (!xmlRootElement)
+		{
+			throw std::runtime_error(filename + " does not contain required root tag of <" + rootElementName + ">");
+		}
+
+		return xmlDocument;
+	}
 }

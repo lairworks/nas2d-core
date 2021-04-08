e81e530a6add86c021c4a757146082fe011e27ed
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-04-08 10:09:22 -0600

    Add unit test for Signal Connection

--- test/Signal.test.cpp
+++ test/Signal.test.cpp
@@ -30,3 +30,21 @@ TEST(Signal, ConnectEmitDisconnect) {
 	EXPECT_CALL(handler, MockMethod()).Times(0);
 	signal.emit();
 }
+
+TEST(Signal, Connection) {
+	MockHandler handler;
+	auto delegate = NAS2D::MakeDelegate(&handler, &MockHandler::MockMethod);
+	NAS2D::Signals::Signal<> signal;
+
+	// Expect a single call to MockMethod
+	EXPECT_CALL(handler, MockMethod()).Times(1);
+
+	{
+		// Connection is only valid in this code block
+		auto connection = decltype(signal)::Connection{signal, delegate};
+		signal.emit();
+	}
+
+	// Disconnected - No additional calls to MockMethod
+	signal.emit();
+}

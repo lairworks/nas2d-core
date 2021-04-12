fe96df0b47bbadc1e5c815b0eb9fc73d1117ca5b
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-04-12 07:44:50 -0600

    Remove default copy/move construction
    Rule of zero/three/five. Removes default methods that may be unsafe in
    typical contexts, and which may be accidentally called.
    
    The Connection object is likely owned by a parent object, with reference
    to the parent object in the delegate. That means if the parent object is
    copied, the contained Connection object should not also be copied, but
    rather reformed with an updated parent object address for the delegate.
    
    By deleting the copy/move constructor/assignment operator, the parent
    object can no longer be default copy/move constructed/assigned, which
    eliminates potential errors that would break connections, or leave
    connections with dangling pointers to the original destructed objects.
    Instead, the developer will get an error about the attempted copy/move
    and be forced to write appropriate copy/move operations that will reform
    the connection for the new object.

--- NAS2D/Connection.h
+++ NAS2D/Connection.h
@@ -14,6 +14,16 @@ public:
 	using SignalType = SignalSource<Params...>;
 	using DelegateType = DelegateX<void, Params...>;
 
+	// No copy/move construction/assignment
+	// This class is designed to handle a parent object's connection to a signal
+	// The delegate parameter is likely tied to the address of the parent object
+	// When parent object is copied/moved, a new updated connection must be formed
+	// Disable copy/move to remove default copy/move from parent objects
+	Connection(const Connection&) = delete;
+	Connection(Connection&&) = delete;
+	Connection& operator=(const Connection&) = delete;
+	Connection& operator=(Connection&&) = delete;
+
 	Connection(SignalType& signalSource, DelegateType delegate) :
 		mSignalSource{signalSource},
 		mDelegate{delegate}

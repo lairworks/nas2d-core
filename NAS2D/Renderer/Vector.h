647e205b9272631dba18e862bd9d64de8eabb3c3
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-06-28 12:15:51 -0600

    Reflow braces in Vector

--- NAS2D/Renderer/Vector.h
+++ NAS2D/Renderer/Vector.h
@@ -5,51 +5,60 @@
 
 namespace NAS2D
 {
-
-
 	template <typename BaseType>
-	struct Vector {
+	struct Vector
+	{
 		BaseType x = 0;
 		BaseType y = 0;
 
-		constexpr bool operator==(const Vector& vector) const {
+		constexpr bool operator==(const Vector& vector) const
+		{
 			return (x == vector.x) && (y == vector.y);
 		}
-		constexpr bool operator!=(const Vector& vector) const {
+
+		constexpr bool operator!=(const Vector& vector) const
+		{
 			return !(*this == vector);
 		}
 
-		Vector& operator+=(const Vector& vector) {
+		Vector& operator+=(const Vector& vector)
+		{
 			x += vector.x;
 			y += vector.y;
 			return *this;
 		}
-		Vector& operator-=(const Vector& vector) {
+		Vector& operator-=(const Vector& vector)
+		{
 			x -= vector.x;
 			y -= vector.y;
 			return *this;
 		}
 
-		constexpr Vector operator+(const Vector& vector) const {
+		constexpr Vector operator+(const Vector& vector) const
+		{
 			return {
 				x + vector.x,
 				y + vector.y
 			};
 		}
-		constexpr Vector operator-(const Vector& vector) const {
+		constexpr Vector operator-(const Vector& vector) const
+		{
 			return {
 				x - vector.x,
 				y - vector.y
 			};
 		}
 
-		Vector& operator*=(BaseType scalar) {
+		Vector& operator*=(BaseType scalar)
+		{
 			x *= scalar;
 			y *= scalar;
 			return *this;
 		}
-		Vector& operator/=(BaseType scalar) {
-			if (scalar == 0) {
+		Vector& operator/=(BaseType scalar)
+		{
+			if (scalar == 0)
+			{
 				throw std::domain_error("Cannot divide vector by 0");
 			}
 			x /= scalar;
@@ -57,47 +66,59 @@ namespace NAS2D
 			return *this;
 		}
 
-		constexpr Vector operator*(BaseType scalar) const {
+		constexpr Vector operator*(BaseType scalar) const
+		{
 			return {x * scalar, y * scalar};
 		}
-		constexpr Vector operator/(BaseType scalar) const {
-			if (scalar == 0) {
+
+		constexpr Vector operator/(BaseType scalar) const
+		{
+			if (scalar == 0)
+			{
 				throw std::domain_error("Cannot divide vector by 0");
 			}
 			return {x / scalar, y / scalar};
 		}
 
-		constexpr Vector skewBy(const Vector& other) const {
+		constexpr Vector skewBy(const Vector& other) const
+		{
 			return {x * other.x, y * other.y};
 		}
 
-		constexpr Vector skewInverseBy(const Vector& other) const {
-			if (other.x == 0 || other.y == 0) {
+		constexpr Vector skewInverseBy(const Vector& other) const
+		{
+			if (other.x == 0 || other.y == 0)
+			{
 				throw std::domain_error("Cannot skewInverseBy a vector with a zero component");
 			}
 			return {x / other.x, y / other.y};
 		}
 
-		constexpr BaseType lengthSquared() const {
+		constexpr BaseType lengthSquared() const
+		{
 			return (x * x) + (y * y);
 		}
 
-		constexpr BaseType dotProduct(const Vector& other) const {
+		constexpr BaseType dotProduct(const Vector& other) const
+		{
 			return (x * other.x) + (y * other.y);
 		}
 
 		// Reflect the x-coordinate (flip across Y-axis)
-		constexpr Vector reflectX() const {
+		constexpr Vector reflectX() const
+		{
 			return {-x, y};
 		}
 
 		// Reflect the y-coordinate (flip across X-axis)
-		constexpr Vector reflectY() const {
+		constexpr Vector reflectY() const
+		{
 			return {x, -y};
 		}
 
 		template <typename NewBaseType>
-		constexpr operator Vector<NewBaseType>() const {
+		constexpr operator Vector<NewBaseType>() const
+		{
 			return {
 				static_cast<NewBaseType>(x),
 				static_cast<NewBaseType>(y)
@@ -105,7 +126,8 @@ namespace NAS2D
 		}
 
 		template <typename NewBaseType>
-		constexpr Vector<NewBaseType> to() const {
+		constexpr Vector<NewBaseType> to() const
+		{
 			return static_cast<Vector<NewBaseType>>(*this);
 		}
 	};
@@ -118,23 +140,26 @@ namespace NAS2D
 	// Partial order comparisons
 
 	template <typename BaseType>
-	bool operator<=(Vector<BaseType> v1, Vector<BaseType> v2) {
+	bool operator<=(Vector<BaseType> v1, Vector<BaseType> v2)
+	{
 		return (v1.x <= v2.x) && (v1.y <= v2.y);
 	}
 
 	template <typename BaseType>
-	bool operator>=(Vector<BaseType> v1, Vector<BaseType> v2) {
+	bool operator>=(Vector<BaseType> v1, Vector<BaseType> v2)
+	{
 		return v2 <= v1;
 	}
 
 	template <typename BaseType>
-	bool operator<(Vector<BaseType> v1, Vector<BaseType> v2) {
+	bool operator<(Vector<BaseType> v1, Vector<BaseType> v2)
+	{
 		return (v1.x < v2.x) && (v1.y < v2.y);
 	}
 
 	template <typename BaseType>
-	bool operator>(Vector<BaseType> v1, Vector<BaseType> v2) {
+	bool operator>(Vector<BaseType> v1, Vector<BaseType> v2)
+	{
 		return v2 < v1;
 	}
-
 }

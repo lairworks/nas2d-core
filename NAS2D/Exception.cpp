7e57e81b5b0048bf9101a901bf4a2216ac26af3b
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-06-28 05:40:29 -0600

    Remove no longer used custom renderer exceptions

--- NAS2D/Exception.cpp
+++ NAS2D/Exception.cpp
@@ -14,23 +14,3 @@
 
 using namespace NAS2D;
 using namespace NAS2D::Exception;
-
-
-renderer_no_glsl::renderer_no_glsl() : runtime_error("Graphics driver reports no GLSL support.")
-{}
-
-
-renderer_backend_init_failure::renderer_backend_init_failure(const std::string& description) : runtime_error("Failure initializing back end video library: " + description)
-{}
-
-
-renderer_backend_init_failure::renderer_backend_init_failure() : runtime_error("Failure initializing back end video library.")
-{}
-
-
-renderer_window_creation_failure::renderer_window_creation_failure() : runtime_error("Unable to create a Window.")
-{}
-
-
-renderer_opengl_context_failure::renderer_opengl_context_failure() : runtime_error("Unable to create an OpenGL Context.")
-{}
--- NAS2D/Exception.h
+++ NAS2D/Exception.h
@@ -22,29 +22,5 @@ namespace NAS2D {
 		 * See <a href="https://github.com/lairworks/nas2d-core/wiki/Exceptions-&-Exception-Handling-in-NAS2D">Wiki Page (Exceptions)</a> for further details.
 		 */
 
-
-		/**
-		 * Thrown when the RendererOpenGL determines that no GLSL shading language is available.
-		 */
-		class renderer_no_glsl : public std::runtime_error { public: renderer_no_glsl(); };
-
-
-		/**
-		 * Thrown when a derived Renderer type is unable to initialize itself.
-		 */
-		class renderer_backend_init_failure : public std::runtime_error { public: renderer_backend_init_failure(const std::string& description); renderer_backend_init_failure();};
-
-
-		/**
-		 * Thrown when a window context can't be created.
-		 */
-		class renderer_window_creation_failure : public std::runtime_error { public: renderer_window_creation_failure(); };
-
-
-		/**
-		 * Thrown when the RendererOpenGL can't create an OpenGL context.
-		 */
-		class renderer_opengl_context_failure : public std::runtime_error { public: renderer_opengl_context_failure(); };
-
 	} // exception namespace
 } // NAS2D namespace

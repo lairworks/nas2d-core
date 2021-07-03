893993621bcfbb5be62f060632421fdeda810a2c
cugone <cugone@gmail.com>
2021-07-03 16:36:58 -0500

    Maintains project code consistency and style

--- NAS2D/Renderer/Window.h
+++ NAS2D/Renderer/Window.h
@@ -3,13 +3,15 @@
 #include <string>
 #include <utility>
 
-enum class WindowDisplayMode {
+enum class WindowDisplayMode
+{
 	Windowed
 	,Borderless
 	,Borderless_Fullscreen
 };
 
-struct WindowDesc {
+struct WindowDesc
+{
 	//The client dimensions
 	//Window creation process should automatically add space for borders, menus and title bars
 	std::pair<int, int> dimensions{1600,600};
@@ -17,40 +19,41 @@ struct WindowDesc {
 	WindowDisplayMode mode{WindowDisplayMode::Windowed};
 };
 
-class Window {
+class Window
+{
 public:
 	virtual ~Window() noexcept {}
 
-	virtual void Open() noexcept = 0;
-	virtual void Close() noexcept = 0;
+	virtual void open() noexcept = 0;
+	virtual void close() noexcept = 0;
 
-	virtual void Show() noexcept = 0;
-	virtual void Hide() noexcept = 0;
-	virtual void Unhide() noexcept = 0;
+	virtual void show() noexcept = 0;
+	virtual void hide() noexcept = 0;
+	virtual void unhide() noexcept = 0;
 
-	[[nodiscard]] virtual bool IsOpen() const noexcept = 0;
-	[[nodiscard]] virtual bool IsClosed() const noexcept = 0;
-	[[nodiscard]] virtual bool IsFullscreen() const noexcept = 0;
-	[[nodiscard]] virtual bool IsWindowed() const noexcept = 0;
+	[[nodiscard]] virtual bool isOpen() const noexcept = 0;
+	[[nodiscard]] virtual bool isClosed() const noexcept = 0;
+	[[nodiscard]] virtual bool isFullscreen() const noexcept = 0;
+	[[nodiscard]] virtual bool isWindowed() const noexcept = 0;
 
 	/**
 	 * @brief Get the underlying OS-specific window handle.
 	 * @return void* Handle to the window. static_cast to appropriate OS-specific type. For Windows this would be HWND.
 	*/
-	[[nodiscard]] virtual void* NativeHandle() const noexcept = 0;
+	[[nodiscard]] virtual void* nativeHandle() const noexcept = 0;
 
 	/**
 	 * @brief Set the underlying OS-specific window handle.
 	 * @param handle: The handle to the window. For Windows this would be a variable of type HWND.
 	 * @return None
 	*/
-	virtual void NativeHandle(void* handle) noexcept = 0;
+	virtual void nativeHandle(void* handle) noexcept = 0;
 
 	/**
 	 * @brief Get the underlying OS-specific window-owned device context.
 	 * @return void* handle to the Device Context. static_cast to appropriate OS-specific type. For Windows this would be HDC.
 	*/
-	[[nodiscard]] virtual void* NativeDeviceContext() const noexcept = 0;
+	[[nodiscard]] virtual void* nativeDeviceContext() const noexcept = 0;
 
 	/**
 	 * @brief Set the underlying OS-specific window-owned device context.
@@ -58,45 +61,44 @@ public:
 	 * @comment For Windows this would be a variable of type HDC.
 	 * @return None
 	*/
-	virtual void NativeDeviceContext(void* hDC) noexcept = 0;
+	virtual void nativeDeviceContext(void* hDC) noexcept = 0;
 
 	/**
 	 * @brief Dimensions of the entire window including any border, menus, or title bars.
 	 * @return std::pair<int, int> x/y components of the window dimensions
 	*/
-	[[nodiscard]] virtual std::pair<int, int> GetDimensions() const noexcept = 0;
-	
+	[[nodiscard]] virtual std::pair<int, int> dimensions() const noexcept = 0;
+
 	/**
 	 * @brief Dimensions of the renderable area of the window not including any borders, menus, or title bars.
 	 * @return std::pair<int, int> x/y components of the client area dimensions
 	*/
-	[[nodiscard]] virtual std::pair<int, int> GetClientDimensions() const noexcept = 0;
+	[[nodiscard]] virtual std::pair<int, int> clientDimensions() const noexcept = 0;
+	virtual void dimensions(std::pair<int, int> newClientDimensions) noexcept = 0;
 
-	virtual void Dimensions(std::pair<int, int> newClientDimensions) noexcept = 0;
+	virtual void title(std::string newTitle) noexcept = 0;
+	[[nodiscard]] virtual std::string title() noexcept = 0;
 
-	virtual void Title(std::string title) noexcept = 0;
-	[[nodiscard]] virtual std::string Title() noexcept = 0;
+	virtual void displayMode(WindowDisplayMode newDisplayMode) noexcept = 0;
+	[[nodiscard]] virtual WindowDisplayMode displayMode() noexcept = 0;
 
-	virtual void DisplayMode(WindowDisplayMode mode) noexcept = 0;
-	[[nodiscard]] virtual WindowDisplayMode DisplayMode() noexcept = 0;
+	[[nodiscard]] virtual std::pair<int, int> position() const noexcept = 0;
+	virtual void position(std::pair<int, int> newPosition) noexcept = 0;
 
-	[[nodiscard]] virtual std::pair<int, int> Position() const noexcept = 0;
-	virtual void Position(std::pair<int, int> newPosition) noexcept = 0;
-
-	virtual void SetDimensionsAndPosition(std::pair<int, int> newClientDimensions, std::pair<int, int> newPosition) noexcept = 0;
+	virtual void setDimensionsAndPosition(std::pair<int, int> newClientDimensions, std::pair<int, int> newPosition) noexcept = 0;
 
 	/**
 	 * @brief Set the native OS-specific window icon resource.
 	 * @param iconResource: Handle to the icon resource.
 	 * @return 
 	*/
-	virtual void Icon(void* iconResource) noexcept = 0;
+	virtual void icon(void* iconResource) noexcept = 0;
 
 	/**
 	 * @brief Get the native OS-specific window icon resource.
 	 * @return void* handle to the native OS-specific window icon resource.
 	*/
-	[[nodiscard]] virtual void* Icon() const noexcept = 0;
+	[[nodiscard]] virtual void* icon() const noexcept = 0;
 protected:
 private:
 	

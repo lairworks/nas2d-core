69fde73e9740c757514e299e8cd6ed4b59e480d8
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-07-15 06:50:27 -0600

    Left justify pointer in type declarations

--- NAS2D/Signal/SignalSource.h
+++ NAS2D/Signal/SignalSource.h
@@ -33,18 +33,18 @@ namespace NAS2D
 		void connect(DelegateType delegate) { delegateList.insert(delegate); }
 
 		template <typename X, typename Y>
-		void connect(Y * obj, void (X::*func)(Params...)) { delegateList.insert(MakeDelegate(obj, func)); }
+		void connect(Y* obj, void (X::*func)(Params...)) { delegateList.insert(MakeDelegate(obj, func)); }
 
 		template <typename X, typename Y>
-		void connect(Y * obj, void (X::*func)(Params...) const) { delegateList.insert(MakeDelegate(obj, func)); }
+		void connect(Y* obj, void (X::*func)(Params...) const) { delegateList.insert(MakeDelegate(obj, func)); }
 
 		void disconnect(DelegateType delegate) { delegateList.erase(delegate); }
 
 		template <typename X, typename Y>
-		void disconnect(Y * obj, void (X::*func)(Params...)) { delegateList.erase(MakeDelegate(obj, func)); }
+		void disconnect(Y* obj, void (X::*func)(Params...)) { delegateList.erase(MakeDelegate(obj, func)); }
 
 		template <typename X, typename Y>
-		void disconnect(Y * obj, void (X::*func)(Params...) const) { delegateList.erase(MakeDelegate(obj, func)); }
+		void disconnect(Y* obj, void (X::*func)(Params...) const) { delegateList.erase(MakeDelegate(obj, func)); }
 
 	protected:
 		std::set<DelegateType> delegateList;

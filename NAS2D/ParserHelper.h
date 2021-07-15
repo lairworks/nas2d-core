8e8374b692d23d0bef8496c9670d00ea396b9491
cugone <cugone@gmail.com>
2021-07-14 20:52:10 -0500

    Adds missing copyright header to NAS2D files

--- NAS2D/ParserHelper.h
+++ NAS2D/ParserHelper.h
@@ -1,3 +1,13 @@
+// ==================================================================================
+// = NAS2D
+// = Copyright © 2008 - 2020 New Age Software
+// ==================================================================================
+// = NAS2D is distributed under the terms of the zlib license. You are free to copy,
+// = modify and distribute the software under the terms of the zlib license.
+// =
+// = Acknowledgment of your use of NAS2D is appreciated but is not required.
+// ==================================================================================
+
 #pragma once
 
 #include "Dictionary.h"

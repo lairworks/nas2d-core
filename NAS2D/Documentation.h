5343a7217d4c28a101a391f49c619f5275138e32
cugone <cugone@gmail.com>
2021-07-14 20:36:13 -0500

    Fixes copyright typos

--- NAS2D/Documentation.h
+++ NAS2D/Documentation.h
@@ -5,7 +5,7 @@
 // = NAS2D is distributed under the terms of the zlib license. You are free to copy,
 // = modify and distribute the software as long under the terms of the zlib license.
 // =
-// = Acknowledgement of your use of NAS2D is appriciated but is not required.
+// = Acknowledgment of your use of NAS2D is appreciated but is not required.
 // ==================================================================================
 
 // This file is purely for documentation purposes and should contain no actual code.
--- NAS2D/Xml/XmlBase.h
+++ NAS2D/Xml/XmlBase.h
@@ -5,7 +5,7 @@
 // = NAS2D is distributed under the terms of the zlib license. You are free to copy,
 // = modify and distribute the software under the terms of the zlib license.
 // =
-// = Acknowledgement of your use of NAS2D is appriciated but is not required.
+// = Acknowledgment of your use of NAS2D is appreciated but is not required.
 // ==================================================================================
 // = Originally based on TinyXML. See Xml.h for additional details.
 // ==================================================================================

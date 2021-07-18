87933d2f4879fd5b0c2eed2cd06672fa13e8419f
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-07-18 06:42:46 -0600

    Convert Sprite unit test to use a test fixture

--- test/Resource/Sprite.test.cpp
+++ test/Resource/Sprite.test.cpp
@@ -1,13 +1,18 @@
 #include "NAS2D/Resource/Sprite.h"
+
 #include <gtest/gtest.h>
 
 
-TEST(Sprite, size) {
+class Sprite : public ::testing::Test {
+protected:
 	uint32_t imageBuffer[1 * 1];
 	NAS2D::Image image{&imageBuffer, 4, {1, 1}};
 	NAS2D::AnimationSet::Frame frame{image, {0, 0, 1, 1}, {0, 0}, 0};
 	NAS2D::AnimationSet animationSet{"", {}, {{"defaultAction", {frame}}}};
 	NAS2D::Sprite sprite{animationSet, "defaultAction"};
+};
 
+
+TEST_F(Sprite, size) {
 	EXPECT_EQ((NAS2D::Vector{1, 1}), sprite.size());
 }

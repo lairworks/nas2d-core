8a32a4dffa80e35cd1717a33fd8fb4c5fbe938d7
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-07-20 01:59:15 -0600

    Add simple Image unit test

--- test/Resource/Image.test.cpp
+++ test/Resource/Image.test.cpp
@@ -1,2 +1,21 @@
 #include "NAS2D/Resource/Image.h"
 #include <gtest/gtest.h>
+
+
+TEST(Image, size) {
+	{
+		uint32_t buffer[1 * 1]{};
+		const auto image = NAS2D::Image{&buffer, 4, {1, 1}};
+		EXPECT_EQ((NAS2D::Vector{1, 1}), image.size());
+	}
+	{
+		uint32_t buffer[2 * 1]{};
+		const auto image = NAS2D::Image{&buffer, 4, {2, 1}};
+		EXPECT_EQ((NAS2D::Vector{2, 1}), image.size());
+	}
+	{
+		uint32_t buffer[1 * 2]{};
+		const auto image = NAS2D::Image{&buffer, 4, {1, 2}};
+		EXPECT_EQ((NAS2D::Vector{1, 2}), image.size());
+	}
+}

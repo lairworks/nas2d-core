2509edf9627f7c2cd8ec22e549f8ccab8e817f42
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-07-20 08:45:06 -0600

    Use a derived Sprite class to allow re-export protected as public

--- test/Resource/Sprite.test.cpp
+++ test/Resource/Sprite.test.cpp
@@ -5,11 +5,19 @@
 
 class Sprite : public ::testing::Test {
 protected:
+	// Use a derived class to access protected methods
+	class SpriteDerived : public NAS2D::Sprite {
+	public:
+		SpriteDerived(const NAS2D::AnimationSet& animationSet, const std::string& initialAction) :
+			Sprite{animationSet, initialAction}
+		{}
+	};
+
 	uint32_t imageBuffer[1 * 1];
 	NAS2D::Image image{&imageBuffer, 4, {1, 1}};
 	NAS2D::AnimationSet::Frame frame{image, {0, 0, 1, 1}, {0, 0}, 0};
 	NAS2D::AnimationSet animationSet{"", {}, {{"defaultAction", {frame}}}};
-	NAS2D::Sprite sprite{animationSet, "defaultAction"};
+	SpriteDerived sprite{animationSet, "defaultAction"};
 };
 
 

186a78a6d96076ffd750a6923ac3a790f512a055
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-12-13 03:40:15 -0700

    Use new XML wrapper code to load `AnimationSet`

--- NAS2D/Resource/AnimationSet.cpp
+++ NAS2D/Resource/AnimationSet.cpp
@@ -19,6 +19,9 @@
 #include "../ParserHelper.h"
 #include "../Version.h"
 #include "../Xml/Xml.h"
+#include "../Parsing/XmlFile.h"
+#include "../Parsing/XmlNamedSubSectionRange.h"
+#include "../Parsing/XmlNamedSubSectionIterator.h"
 
 #include <tuple>
 
@@ -33,21 +36,14 @@ namespace
 	using ImageCache = ResourceCache<Image, std::string>;
 	ImageCache animationImageCache;
 
-
-	// Adds a row tag to the end of messages.
-	std::string endTag(int row)
-	{
-		return " (Row: " + std::to_string(row) + ")";
-	}
-
 	using ImageSheetMap = AnimationSet::ImageSheetMap;
 	using ActionsMap = AnimationSet::ActionsMap;
 
 
 	std::tuple<ImageSheetMap, ActionsMap> processXml(const std::string& filePath, ImageCache& imageCache);
-	ImageSheetMap processImageSheets(const std::string& basePath, const Xml::XmlElement* element, ImageCache& imageCache);
-	ActionsMap processActions(const ImageSheetMap& imageSheetMap, const Xml::XmlElement* element, ImageCache& imageCache);
-	std::vector<AnimationSet::Frame> processFrames(const ImageSheetMap& imageSheetMap, const Xml::XmlElement* element, ImageCache& imageCache);
+	ImageSheetMap processImageSheets(const std::string& basePath, const XmlSection& spriteSection, ImageCache& imageCache);
+	ActionsMap processActions(const ImageSheetMap& imageSheetMap, const XmlSection& spriteSection, ImageCache& imageCache);
+	std::vector<AnimationSet::Frame> processFrames(const ImageSheetMap& imageSheetMap, const XmlSection& actionSection, ImageCache& imageCache);
 }
 
 
@@ -116,23 +112,10 @@ namespace
 			auto& filesystem = Utility<Filesystem>::get();
 			const auto basePath = filesystem.parentPath(filePath);
 
-			Xml::XmlDocument xmlDoc;
-			xmlDoc.parse(filesystem.read(filePath).c_str());
+			const auto xmlFile = XmlFile{filesystem.read(filePath)};
+			const auto spriteSection = xmlFile.root("sprite");
 
-			if (xmlDoc.error())
-			{
-				throw std::runtime_error("Sprite file has malformed XML: Row: " + std::to_string(xmlDoc.errorRow()) + " Column: " + std::to_string(xmlDoc.errorCol()) + " : " + xmlDoc.errorDesc());
-			}
-
-			// Find the Sprite node.
-			const auto* xmlRootElement = xmlDoc.firstChildElement("sprite");
-			if (!xmlRootElement)
-			{
-				throw std::runtime_error("Sprite file does not contain required <sprite> tag");
-			}
-
-			// Get the Sprite version.
-			const auto version = xmlRootElement->attribute("version");
+			const auto version = spriteSection.value("version", std::string{""});
 			if (version.empty())
 			{
 				throw std::runtime_error("Sprite file's root element does not specify a version");
@@ -146,8 +129,8 @@ namespace
 			// Here instead of going through each element and calling a processing function to handle
 			// it, we just iterate through all nodes to find sprite sheets. This allows us to define
 			// image sheets anywhere in the sprite file.
-			auto imageSheetMap = processImageSheets(basePath, xmlRootElement, imageCache);
-			auto actions = processActions(imageSheetMap, xmlRootElement, imageCache);
+			auto imageSheetMap = processImageSheets(basePath, spriteSection, imageCache);
+			auto actions = processActions(imageSheetMap, spriteSection, imageCache);
 			return std::tuple{std::move(imageSheetMap), std::move(actions)};
 		}
 		catch(const std::runtime_error& error)
@@ -165,29 +148,28 @@ namespace
 	 *			element in a sprite definition, these elements can appear
 	 *			anywhere in a Sprite XML definition.
 	 */
-	ImageSheetMap processImageSheets(const std::string& basePath, const Xml::XmlElement* element, ImageCache& imageCache)
+	ImageSheetMap processImageSheets(const std::string& basePath, const XmlSection& spriteSection, ImageCache& imageCache)
 	{
 		ImageSheetMap imageSheetMap;
 
-		for (const auto* node = element->firstChildElement("imagesheet"); node; node = node->nextSiblingElement("imagesheet"))
+		for (const auto& section : spriteSection.subSections("imagesheet"))
 		{
-			const auto dictionary = attributesToDictionary(*node);
-			const auto id = dictionary.get("id");
-			const auto src = dictionary.get("src");
+			const auto id = section.value("id");
+			const auto src = section.value("src");
 
 			if (id.empty())
 			{
-				throw std::runtime_error("Sprite imagesheet definition has `id` of length zero: " + endTag(node->row()));
+				section.throwError("Sprite imagesheet definition has `id` of length zero");
 			}
 
 			if (src.empty())
 			{
-				throw std::runtime_error("Sprite imagesheet definition has `src` of length zero: " + endTag(node->row()));
+				section.throwError("Sprite imagesheet definition has `src` of length zero");
 			}
 
 			if (imageSheetMap.find(id) != imageSheetMap.end())
 			{
-				throw std::runtime_error("Sprite image sheet redefinition: id: '" + id + "' " + endTag(node->row()));
+				section.throwError("Sprite image sheet redefinition: id: '" + id + "'");
 			}
 
 			const auto imagePath = basePath + src;
@@ -203,29 +185,28 @@ namespace
 	 * Iterates through all elements of a Sprite XML definition looking
 	 * for 'action' elements and processes them.
 	 */
-	ActionsMap processActions(const ImageSheetMap& imageSheetMap, const Xml::XmlElement* element, ImageCache& imageCache)
+	ActionsMap processActions(const ImageSheetMap& imageSheetMap, const XmlSection& spriteSection, ImageCache& imageCache)
 	{
 		ActionsMap actions;
 
-		for (const auto* action = element->firstChildElement("action"); action; action = action->nextSiblingElement("action"))
+		for (const auto& section : spriteSection.subSections("action"))
 		{
-			const auto dictionary = attributesToDictionary(*action);
-			const auto actionName = dictionary.get("name");
+			const auto actionName = section.value("name");
 
 			if (actionName.empty())
 			{
-				throw std::runtime_error("Sprite Action definition has 'name' of length zero: " + endTag(action->row()));
+				section.throwError("Sprite Action definition has 'name' of length zero");
 			}
 			if (actions.find(actionName) != actions.end())
 			{
-				throw std::runtime_error("Sprite Action redefinition: '" + actionName + "' " + endTag(action->row()));
+				section.throwError("Sprite Action redefinition: '" + actionName + "'");
 			}
 
-			actions[actionName] = processFrames(imageSheetMap, action, imageCache);
+			actions[actionName] = processFrames(imageSheetMap, section, imageCache);
 
 			if (actions[actionName].empty())
 			{
-				throw std::runtime_error("Sprite Action contains no valid frames: " + actionName);
+				section.throwError("Sprite Action contains no valid frames: " + actionName);
 			}
 		}
 
@@ -236,56 +217,53 @@ namespace
 	/**
 	 * Parses through all <frame> tags within an <action> tag in a Sprite Definition.
 	 */
-	std::vector<AnimationSet::Frame> processFrames(const ImageSheetMap& imageSheetMap, const Xml::XmlElement* element, ImageCache& imageCache)
+	std::vector<AnimationSet::Frame> processFrames(const ImageSheetMap& imageSheetMap, const XmlSection& actionSection, ImageCache& imageCache)
 	{
 		std::vector<AnimationSet::Frame> frameList;
 
-		for (const auto* frame = element->firstChildElement("frame"); frame; frame = frame->nextSiblingElement("frame"))
+		for (const auto& section : actionSection.subSections("frame"))
 		{
-			int currentRow = frame->row();
+			section.verifyKeys({"sheetid", "x", "y", "width", "height", "anchorx", "anchory"}, {"delay"});
 
-			const auto dictionary = attributesToDictionary(*frame);
-			reportMissingOrUnexpected(dictionary.keys(), {"sheetid", "x", "y", "width", "height", "anchorx", "anchory"}, {"delay"});
-
-			const auto sheetId = dictionary.get("sheetid");
-			const auto delay = dictionary.get<unsigned int>("delay", 0);
-			const auto x = dictionary.get<int>("x");
-			const auto y = dictionary.get<int>("y");
-			const auto width = dictionary.get<int>("width");
-			const auto height = dictionary.get<int>("height");
-			const auto anchorx = dictionary.get<int>("anchorx");
-			const auto anchory = dictionary.get<int>("anchory");
+			const auto sheetId = section.value("sheetid");
+			const auto delay = section.value<unsigned int>("delay", 0);
+			const auto x = section.value<int>("x");
+			const auto y = section.value<int>("y");
+			const auto width = section.value<int>("width");
+			const auto height = section.value<int>("height");
+			const auto anchorx = section.value<int>("anchorx");
+			const auto anchory = section.value<int>("anchory");
 
 			if (sheetId.empty())
 			{
-				throw std::runtime_error("Sprite Frame definition has 'sheetid' of length zero: " + endTag(currentRow));
+				section.throwError("Sprite Frame definition has 'sheetid' of length zero");
 			}
 			const auto iterator = imageSheetMap.find(sheetId);
 			if (iterator == imageSheetMap.end())
 			{
-				throw std::runtime_error("Sprite Frame definition references undefined imagesheet: '" + sheetId + "' " + endTag(currentRow));
+				section.throwError("Sprite Frame definition references undefined imagesheet: '" + sheetId + "'");
 			}
 
 			const auto& image = imageCache.load(iterator->second);
 			// X-Coordinate
 			if (x < 0 || x > image.size().x)
 			{
-				throw std::runtime_error("Sprite frame attribute 'x' is out of bounds: " + endTag(currentRow));
+				section.throwError("Sprite frame attribute 'x' is out of bounds");
 			}
 			// Y-Coordinate
 			if (y < 0 || y > image.size().y)
 			{
-				throw std::runtime_error("Sprite frame attribute 'y' is out of bounds: " + endTag(currentRow));
+				section.throwError("Sprite frame attribute 'y' is out of bounds");
 			}
 			// Width
 			if (width <= 0 || width > image.size().x - x)
 			{
-				throw std::runtime_error("Sprite frame attribute 'width' is out of bounds: " + endTag(currentRow));
+				section.throwError("Sprite frame attribute 'width' is out of bounds");
 			}
 			// Height
 			if (height <= 0 || height > image.size().y - y)
 			{
-				throw std::runtime_error("Sprite frame attribute 'height' is out of bounds: " + endTag(currentRow));
+				section.throwError("Sprite frame attribute 'height' is out of bounds");
 			}
 
 			const auto bounds = Rectangle<int>::Create(Point<int>{x, y}, Vector{width, height});

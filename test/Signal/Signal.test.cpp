9e58ab1dfff96a95348a8a295287c38ec7083042
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-01-02 05:42:46 -0700

    Split `Signal` unit tests (one expectation per test)

--- test/Signal/Signal.test.cpp
+++ test/Signal/Signal.test.cpp
@@ -12,21 +12,38 @@ namespace {
 }
 
 
-TEST(Signal, ConnectEmitDisconnect) {
+TEST(Signal, InitEmpty) {
+	NAS2D::Signal<> signal;
+	EXPECT_TRUE(signal.empty());
+}
+
+TEST(Signal, ConnectNonEmpty) {
+	NAS2D::Signal<> signal;
 	MockHandler handler{};
 	auto delegate = NAS2D::Delegate{&handler, &MockHandler::MockMethod};
-	NAS2D::Signal<> signal;
-
-	EXPECT_TRUE(signal.empty());
 
 	signal.connect(delegate);
 	EXPECT_FALSE(signal.empty());
+}
 
-	EXPECT_CALL(handler, MockMethod());
-	signal.emit();
+TEST(Signal, DisconnectEmpty) {
+	NAS2D::Signal<> signal;
+	MockHandler handler{};
+	auto delegate = NAS2D::Delegate{&handler, &MockHandler::MockMethod};
 
+	signal.connect(delegate);
 	signal.disconnect(delegate);
 	EXPECT_TRUE(signal.empty());
+}
 
+TEST(Signal, ConnectEmitDisconnect) {
+	NAS2D::Signal<> signal;
+	MockHandler handler{};
+	auto delegate = NAS2D::Delegate{&handler, &MockHandler::MockMethod};
+
+	EXPECT_CALL(handler, MockMethod()).Times(1);
+	signal.connect(delegate);
+	signal.emit();
+	signal.disconnect(delegate);
 	signal.emit();
 }

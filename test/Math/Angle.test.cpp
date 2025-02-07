87a6149489c765a0c14593a3c9636625333a96ee
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-06 21:32:02 -0700

    Add unit tests for `Angle` operators

--- test/Math/Angle.test.cpp
+++ test/Math/Angle.test.cpp
@@ -19,3 +19,18 @@ TEST(Angle, degreesToRadians) {
 	constexpr auto angle = NAS2D::Angle::degrees(180.0f);
 	EXPECT_FLOAT_EQ(std::numbers::pi_v<float>, angle.radians());
 }
+
+TEST(Angle, unaryMinus) {
+	constexpr auto angle = -NAS2D::Angle::degrees(180.0f);
+	EXPECT_FLOAT_EQ(-180.0f, angle.degrees());
+}
+
+TEST(Angle, addition) {
+	constexpr auto angle = NAS2D::Angle::degrees(90.0f) + NAS2D::Angle::degrees(90.0f);
+	EXPECT_FLOAT_EQ(180.0f, angle.degrees());
+}
+
+TEST(Angle, subtraction) {
+	constexpr auto angle = NAS2D::Angle::degrees(180.0f) - NAS2D::Angle::degrees(90.0f);
+	EXPECT_FLOAT_EQ(90.0f, angle.degrees());
+}

02afa06a4219481be724f2dcab251afae3e835fd
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-06 20:16:58 -0700

    Expand `Angle` to full class with static constructors
    In hindsight, using a `struct` with a single `public` member is not sufficient to ensure correct usage. Althugh it did introduce a new type, which could be used to increase type safety, it didn't address the distinction between degrees and radians. By using a `class`, and making the single field and constructor `private`, we can control construction using named `static` constructors. This ensures values are always explicitly specified as either degrees or radians.
    
    Note that `Angle` is still a wrapper for a single primitive value. The aim of this class is to have a value that is easy and efficient to pass around, much like a primitive value, but with additional compile time type safety.

--- NAS2D/Math/Angle.h
+++ NAS2D/Math/Angle.h
@@ -1,10 +1,31 @@
 #pragma once
 
+#include <numbers>
+
 
 namespace NAS2D
 {
-	struct Angle
+	class Angle
 	{
-		float radians;
+	protected:
+		// Non-public constructor forces the use of static factory methods
+		constexpr Angle(float radians) : radianMeasure{radians} {}
+
+		static constexpr float DegreeToRadians = std::numbers::pi_v<float> / 180;
+		static constexpr float RadiansToDegrees = 180 / std::numbers::pi_v<float>;
+
+		static constexpr float degToRad(float degrees) { return degrees * DegreeToRadians; }
+		static constexpr float radToDeg(float radians) { return radians * RadiansToDegrees; }
+
+	public:
+		// Static factory methods, with an explicit angle measure
+		static constexpr Angle degrees(float degrees) { return Angle{degToRad(degrees)}; }
+		static constexpr Angle radians(float radians) { return Angle{radians}; }
+
+		constexpr float degrees() const { return radToDeg(radianMeasure); }
+		constexpr float radians() const { return radianMeasure; }
+
+	private:
+		float radianMeasure;
 	};
 }

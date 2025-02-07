73709110fafc6b134a7e00d920b410f435cbd0f5
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-06 20:27:46 -0700

    Add `Angle` unit tests

---
+++ test/Math/Angle.test.cpp
@@ -0,0 +1,21 @@
+#include "NAS2D/Math/Angle.h"
+
+#include <gtest/gtest.h>
+
+#include <numbers>
+
+
+TEST(Angle, degrees) {
+	constexpr auto angle = NAS2D::Angle::degrees(90.0f);
+	EXPECT_FLOAT_EQ(90.0f, angle.degrees());
+}
+
+TEST(Angle, radians) {
+	constexpr auto angle = NAS2D::Angle::radians(1.0f);
+	EXPECT_FLOAT_EQ(1.0f, angle.radians());
+}
+
+TEST(Angle, degreesToRadians) {
+	constexpr auto angle = NAS2D::Angle::degrees(180.0f);
+	EXPECT_FLOAT_EQ(std::numbers::pi_v<float>, angle.radians());
+}

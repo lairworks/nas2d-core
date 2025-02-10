9ae27f81b15132257607c4c365e20895576781ec
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-09 21:45:33 -0700

    Use degrees instead of radians and `PI_2` constant in `drawCircle`
    I initially read the `PI_2` constant as `Pi / 2`, instead of `Pi * 2`. It helps a little to have the definition moved to where it's being used. Though using `Angle::degrees(360)` is far more clear.

--- NAS2D/Renderer/RendererOpenGL.cpp
+++ NAS2D/Renderer/RendererOpenGL.cpp
@@ -365,10 +365,7 @@ void RendererOpenGL::drawCircle(Point<float> position, float radius, Color color
 	glDisable(GL_TEXTURE_2D);
 	setColor(color);
 
-	constexpr float PI = 3.14159265f;
-	constexpr float PI_2 = PI * 2;
-
-	const auto theta = Angle::radians(PI_2) / static_cast<float>(numSegments);
+	const auto theta = Angle::degrees(360) / static_cast<float>(numSegments);
 	const auto direction = getDirectionVector(theta);
 	const auto cosTheta = direction.x;
 	const auto sinTheta = direction.y;

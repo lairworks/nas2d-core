18fd5f86e9940ea69f68d9a4c82681fc1765d49a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-18 10:45:57 -0700

    Add `SignalSource::count()` tests

--- test/Signal/SignalSource.test.cpp
+++ test/Signal/SignalSource.test.cpp
@@ -12,6 +12,32 @@ namespace {
 }
 
 
+TEST(SignalSource, InitCountZero) {
+	NAS2D::SignalSource<> signalSource;
+	EXPECT_EQ(std::size_t{0}, signalSource.count());
+}
+
+TEST(SignalSource, ConnectCountOne) {
+	NAS2D::SignalSource<> signalSource;
+	MockHandler handler{};
+	auto delegate = NAS2D::Delegate{&handler, &MockHandler::MockMethod};
+
+	signalSource.connect(delegate);
+	EXPECT_EQ(std::size_t{1}, signalSource.count());
+}
+
+TEST(SignalSource, ConnectCountTwo) {
+	NAS2D::SignalSource<> signalSource;
+	MockHandler handler1{};
+	MockHandler handler2{};
+	auto delegate1 = NAS2D::Delegate{&handler1, &MockHandler::MockMethod};
+	auto delegate2 = NAS2D::Delegate{&handler2, &MockHandler::MockMethod};
+
+	signalSource.connect(delegate1);
+	signalSource.connect(delegate2);
+	EXPECT_EQ(std::size_t{2}, signalSource.count());
+}
+
 TEST(SignalSource, InitEmpty) {
 	NAS2D::SignalSource<> signalSource;
 	EXPECT_TRUE(signalSource.isEmpty());

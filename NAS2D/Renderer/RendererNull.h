ea0d366e87cafc50162f2dddabad13151f0303e3
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-03-01 06:55:05 -0700

    Move `RendererNull` implementation to source file
    This creates out-of-line virtual method definitions. This removes an instance of the Clang warning `-Wweak-vtables`.

--- NAS2D/Renderer/RendererNull.h
+++ NAS2D/Renderer/RendererNull.h
@@ -22,40 +22,40 @@ namespace NAS2D
 	public:
 		RendererNull() = default;
 
-		~RendererNull() override {}
+		~RendererNull() override;
 
-		void drawImage(const Image&, Point<float>, float = 1.0, Color = Color::Normal) override {}
+		void drawImage(const Image&, Point<float>, float = 1.0, Color = Color::Normal) override;
 
-		void drawSubImage(const Image&, Point<float>, const Rectangle<float>&, Color = Color::Normal) override {}
-		void drawSubImageRotated(const Image&, Point<float>, const Rectangle<float>&, Angle, Color = Color::Normal) override {}
+		void drawSubImage(const Image&, Point<float>, const Rectangle<float>&, Color = Color::Normal) override;
+		void drawSubImageRotated(const Image&, Point<float>, const Rectangle<float>&, Angle, Color = Color::Normal) override;
 
-		void drawImageRotated(const Image&, Point<float>, Angle, Color = Color::Normal, float = 1.0f) override {}
-		void drawImageStretched(const Image&, const Rectangle<float>&, Color = Color::Normal) override {}
+		void drawImageRotated(const Image&, Point<float>, Angle, Color = Color::Normal, float = 1.0f) override;
+		void drawImageStretched(const Image&, const Rectangle<float>&, Color = Color::Normal) override;
 
-		void drawImageRepeated(const Image&, const Rectangle<float>&) override {}
-		void drawSubImageRepeated(const Image&, const Rectangle<float>&, const Rectangle<float>&) override {}
+		void drawImageRepeated(const Image&, const Rectangle<float>&) override;
+		void drawSubImageRepeated(const Image&, const Rectangle<float>&, const Rectangle<float>&) override;
 
-		void drawImageToImage(const Image&, const Image&, Point<float>) override {}
+		void drawImageToImage(const Image&, const Image&, Point<float>) override;
 
-		void drawPoint(Point<float>, Color = Color::White) override {}
-		void drawLine(Point<float>, Point<float>, Color = Color::White, int = 1) override {}
-		void drawBox(const Rectangle<float>&, Color = Color::White) override {}
-		void drawBoxFilled(const Rectangle<float>&, Color = Color::White) override {}
-		void drawCircle(Point<float>, float, Color, int = 10, Vector<float> = Vector{1.0f, 1.0f}) override {}
+		void drawPoint(Point<float>, Color = Color::White) override;
+		void drawLine(Point<float>, Point<float>, Color = Color::White, int = 1) override;
+		void drawBox(const Rectangle<float>&, Color = Color::White) override;
+		void drawBoxFilled(const Rectangle<float>&, Color = Color::White) override;
+		void drawCircle(Point<float>, float, Color, int = 10, Vector<float> = Vector{1.0f, 1.0f}) override;
 
-		void drawGradient(const Rectangle<float>&, Color, Color, Color, Color) override {}
+		void drawGradient(const Rectangle<float>&, Color, Color, Color, Color) override;
 
-		void drawText(const Font&, std::string_view, Point<float>, Color = Color::White) override {}
+		void drawText(const Font&, std::string_view, Point<float>, Color = Color::White) override;
 
-		void clearScreen(Color = Color::Black) override {}
+		void clearScreen(Color = Color::Black) override;
 
-		void clipRect(const Rectangle<float>&) override {}
-		void clipRectClear() override {}
+		void clipRect(const Rectangle<float>&) override;
+		void clipRectClear() override;
 
-		void update() override {}
+		void update() override;
 
-		void setViewport(const Rectangle<int>&) override {}
-		void setOrthoProjection(const Rectangle<float>&) override {}
+		void setViewport(const Rectangle<int>&) override;
+		void setOrthoProjection(const Rectangle<float>&) override;
 	};
 
 } // namespace NAS2D

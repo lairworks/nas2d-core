259b756473e9d070d6305595ac6d6f61d826724e
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-09-20 00:39:42 -0600

    Add unit tests for `Duration` comparison operators

---
+++ test/Duration.test.cpp
@@ -0,0 +1,18 @@
+#include "NAS2D/Duration.h"
+
+#include <gtest/gtest.h>
+
+
+TEST(Duration, OperatorCompare) {
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{0});
+	EXPECT_NE(NAS2D::Duration{0}, NAS2D::Duration{1});
+
+	EXPECT_LT(NAS2D::Duration{0}, NAS2D::Duration{1});
+	EXPECT_GT(NAS2D::Duration{1}, NAS2D::Duration{0});
+
+	EXPECT_LE(NAS2D::Duration{0}, NAS2D::Duration{0});
+	EXPECT_LE(NAS2D::Duration{0}, NAS2D::Duration{1});
+
+	EXPECT_GE(NAS2D::Duration{0}, NAS2D::Duration{0});
+	EXPECT_GE(NAS2D::Duration{1}, NAS2D::Duration{0});
+}

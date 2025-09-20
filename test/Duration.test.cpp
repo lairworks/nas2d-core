fe2ab3bdf10081260a6ed01d624837448e2c7a2e
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-09-20 00:47:54 -0600

    Add unit tests for `Duration` operators `*`, `/`, and `%`

--- test/Duration.test.cpp
+++ test/Duration.test.cpp
@@ -30,3 +30,25 @@ TEST(Duration, OperatorMinus) {
 	EXPECT_EQ(NAS2D::Duration{1}, NAS2D::Duration{1} - NAS2D::Duration{0});
 	EXPECT_EQ(NAS2D::Duration{1}, NAS2D::Duration{2} - NAS2D::Duration{1});
 }
+
+TEST(Duration, OperatorMultiply) {
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{0} * 0);
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{1} * 0);
+	EXPECT_EQ(NAS2D::Duration{1}, NAS2D::Duration{1} * 1);
+	EXPECT_EQ(NAS2D::Duration{4}, NAS2D::Duration{2} * 2);
+}
+
+TEST(Duration, OperatorDivide) {
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{0} / 1);
+	EXPECT_EQ(NAS2D::Duration{1}, NAS2D::Duration{1} / 1);
+	EXPECT_EQ(NAS2D::Duration{2}, NAS2D::Duration{2} / 1);
+	EXPECT_EQ(NAS2D::Duration{2}, NAS2D::Duration{4} / 2);
+}
+
+TEST(Duration, OperatorModulo) {
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{0} % 1);
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{1} % 1);
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{0} % 2);
+	EXPECT_EQ(NAS2D::Duration{1}, NAS2D::Duration{1} % 2);
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{2} % 2);
+}

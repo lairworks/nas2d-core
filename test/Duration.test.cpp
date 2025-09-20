6ff8465268d12714edd6e17e183a829ba4171fb0
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-09-20 00:42:52 -0600

    Add unit tests for `Duration` operators `+` and `-`

--- test/Duration.test.cpp
+++ test/Duration.test.cpp
@@ -16,3 +16,17 @@ TEST(Duration, OperatorCompare) {
 	EXPECT_GE(NAS2D::Duration{0}, NAS2D::Duration{0});
 	EXPECT_GE(NAS2D::Duration{1}, NAS2D::Duration{0});
 }
+
+TEST(Duration, OperatorPlus) {
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{0} + NAS2D::Duration{0});
+	EXPECT_EQ(NAS2D::Duration{1}, NAS2D::Duration{0} + NAS2D::Duration{1});
+	EXPECT_EQ(NAS2D::Duration{1}, NAS2D::Duration{1} + NAS2D::Duration{0});
+	EXPECT_EQ(NAS2D::Duration{2}, NAS2D::Duration{1} + NAS2D::Duration{1});
+}
+
+TEST(Duration, OperatorMinus) {
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{0} - NAS2D::Duration{0});
+	EXPECT_EQ(NAS2D::Duration{0}, NAS2D::Duration{1} - NAS2D::Duration{1});
+	EXPECT_EQ(NAS2D::Duration{1}, NAS2D::Duration{1} - NAS2D::Duration{0});
+	EXPECT_EQ(NAS2D::Duration{1}, NAS2D::Duration{2} - NAS2D::Duration{1});
+}

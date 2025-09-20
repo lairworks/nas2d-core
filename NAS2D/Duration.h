1732fe7827662eab0229a3e4bb961cb8b27cf2d0
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-09-20 00:37:36 -0600

    Add `Duration` comparison operators

--- NAS2D/Duration.h
+++ NAS2D/Duration.h
@@ -9,4 +9,13 @@ namespace NAS2D
 	{
 		uint32_t milliseconds;
 	};
+
+
+	inline bool operator==(Duration duration1, Duration duration2) { return duration1.milliseconds == duration2.milliseconds; }
+	inline bool operator!=(Duration duration1, Duration duration2) { return duration1.milliseconds != duration2.milliseconds; }
+
+	inline bool operator<(Duration duration1, Duration duration2) { return duration1.milliseconds < duration2.milliseconds; }
+	inline bool operator>(Duration duration1, Duration duration2) { return duration1.milliseconds > duration2.milliseconds; }
+	inline bool operator<=(Duration duration1, Duration duration2) { return duration1.milliseconds <= duration2.milliseconds; }
+	inline bool operator>=(Duration duration1, Duration duration2) { return duration1.milliseconds >= duration2.milliseconds; }
 }

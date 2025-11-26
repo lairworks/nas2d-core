cb105bf0c526587fa2395a4386a82567ca1ee48b
br3nt <brent.jacobs@gmail.com>
2025-11-26 19:14:31 +1030

    Use OpenGL Compatibility Profile instead of Core Profile
    The renderer uses legacy fixed-function OpenGL (glColor4ub, glPushMatrix,
    glVertexPointer, etc.) which requires Compatibility Profile. Core Profile
    3.2+ removed these functions, causing rendering failure on macOS which
    strictly enforces profile restrictions.
    
    Fixes OutpostUniverse/OPHD#2173

--- NAS2D/Renderer/RendererOpenGL.cpp
+++ NAS2D/Renderer/RendererOpenGL.cpp
@@ -607,7 +607,7 @@ void RendererOpenGL::initSdl(Vector<int> resolution, bool fullscreen)
 
 void RendererOpenGL::initSdlGL(bool vsync)
 {
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 4);
